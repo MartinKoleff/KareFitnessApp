f4775fb8599214af6e26954fc04d476c4db9ff40
Gabriel Genkov <genkovgg@gmail.com>
2023-11-25 16:25:18 +0200

    fixed one small mapping issue

--- back-end/src/main/java/io/kare/backend/mapper/UserMapper.java
+++ back-end/src/main/java/io/kare/backend/mapper/UserMapper.java
@@ -4,9 +4,11 @@ import io.kare.backend.entity.UserEntity;
 import io.kare.backend.payload.request.RegisterUserRequest;
 import io.kare.backend.payload.response.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+	@Mapping(target = "password", source = "password")
 	UserEntity map(RegisterUserRequest request, String password);
 	RegisterUserResponse mapRegisterUserResponse(UserEntity entity, String token);
 	LoginUserResponse mapLoginUserResponse(UserEntity entity, String token);
