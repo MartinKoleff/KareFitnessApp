0fda3c493555c9b13e5cd512fb33d2bb5eace2fb
Chavdar <cavdar2002@gmail.com>
2023-11-29 01:34:07 +0200

    main-page-style-done

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {HomeComponent} from './home/home.component'
 import {LoginComponent} from './login/login.component'
 import { RegisterComponent } from './register/register.component';
 import { StartupComponent } from './startup/startup.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  {path:'', component:StartupComponent},
+  {path:'startup', component:StartupComponent},
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
-  {path:'home', component: HomeComponent}
+  {path:'home', component: HomeComponent},
+  {path:'main', component: MainComponent}
 ];
 
 @NgModule({
