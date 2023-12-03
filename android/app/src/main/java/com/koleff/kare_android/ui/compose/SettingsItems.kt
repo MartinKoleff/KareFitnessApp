9891cf346f9795d08615eb6c56ad067f5b57b7ba
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 23:19:49 +0200

    Added settings item composables.

---
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/SettingsItems.kt
@@ -0,0 +1,68 @@
+package com.koleff.kare_android.ui.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import com.koleff.kare_android.R
+
+@Composable
+fun SettingsListItem(title: String, icon: ImageVector, description: String) {
+    ListItem(
+        headlineContent = { Text(title) },
+        leadingContent = {
+            Icon(
+                icon,
+                contentDescription = description,
+            )
+        }
+    )
+    HorizontalDivider()
+}
+
+@Composable
+fun SettingsListItem(title: String, icon: Painter, description: String) {
+    ListItem(
+        headlineContent = { Text(title) },
+        leadingContent = {
+            Icon(
+                icon,
+                contentDescription = description,
+            )
+        }
+    )
+    HorizontalDivider()
+}
+
+@Composable
+fun SettingsCategory(title: String) {
+
+}
+
+@Composable
+fun SettingsList(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        SettingsListItem("Logout", painterResource(R.drawable.ic_vector_logout), "Logout from account")
+        SettingsListItem("Change email", Icons.Default.Email, "Change login email")
+        SettingsListItem("Change password", painterResource(R.drawable.ic_vector_password), "Change password")
+
+        SettingsListItem("Push notifications", Icons.Default.Notifications, "Push notifications settings")
+        SettingsListItem("Change language", painterResource(R.drawable.ic_vector_language), "Change language") //TODO: add switcher view
+        SettingsListItem("Biometric authentication", painterResource(R.drawable.ic_faceid), "Biometric authentication")
+
+        SettingsCategory("Privacy policy")
+        SettingsListItem("Privacy policy", Icons.Default.Info, "Privacy policy")
+        SettingsListItem("Support", Icons.Default.MailOutline, "Contact us via email")
+    }
+}
