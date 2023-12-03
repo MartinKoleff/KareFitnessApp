1bc19d0a70eb84e15d8313509e593187a5bf07ee
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-03 20:56:23 +0200

    Added all main screens.

---
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/screen/ExerciseDetailsScreen.kt
@@ -0,0 +1,12 @@
+package com.koleff.kare_android.ui.compose.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ExerciseDetailsScreen(
+    navController: NavHostController,
+    exerciseId: Int = -1 //Invalid exercise selected...
+) {
+
+}
