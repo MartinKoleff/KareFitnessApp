656f7d7cab1db591618d190483bec0958d30fe1d
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-05 12:25:51 +0200

    Wired getExerciseResponse with baseResponse.

--- android/app/src/main/java/com/koleff/kare_android/data/model/response/GetExercisesResponse.kt
+++ android/app/src/main/java/com/koleff/kare_android/data/model/response/GetExercisesResponse.kt
@@ -1,4 +1,12 @@
 package com.koleff.kare_android.data.model.response
 
-class GetExercisesResponse {
-}
+import com.koleff.kare_android.data.model.dto.ExerciseDto
+import com.koleff.kare_android.data.model.response.base_response.BaseResponse
+import com.squareup.moshi.Json
+
+data class GetExercisesResponse(
+    //    @Json(name = "pagination")
+//    val paginationData: PaginationDataDto,
+    @Json(name = "data")
+    val exercisesList: List<ExerciseDto>
+) : BaseResponse()
