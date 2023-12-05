3fdff7947cdddffcc34223a8ec080114ff3c0a64
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-05 13:55:03 +0200

    Added is navigation in transition logic.
    When button for navigation is pressed 750ms start counting and during this time any navigation is blocked. This helps make navigation smooth and wait for its animation. If this is not added you can spam navigate thru screens. After testing 750ms is ideal time.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/navigation/NavigationItem.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/navigation/NavigationItem.kt
@@ -4,30 +4,57 @@ import android.util.Log
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import com.koleff.kare_android.data.MainScreen
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun NavigationItem(
     navController: NavHostController,
     screen: MainScreen?, //When screen is null -> navigate to latest backstack entry after popping the details screen
     icon: Any, //Can be Painter or ImageVector
-    label: String
+    label: String,
+    isBlocked: MutableState<Boolean>
 ) {
+    LaunchedEffect(key1 = isBlocked.value) {
+        Log.d(
+            "Navigation LaunchedEffect",
+            "Is navigation in progress: ${isBlocked.value}"
+        )
+
+        if (isBlocked.value) {
+            blockNavigationButtons(isBlocked)
+        }
+    }
+
     IconButton(
         onClick = {
             screen?.let {
-                if(navController.currentBackStackEntry!!.destination.route == screen.route) return@IconButton
-
-                navController.navigate(screen.route)
-
                 Log.d(
                     "Navigation",
-                    "Backstack: ${navController.currentBackStackEntry}, ${navController.currentBackStack.value.size}"
+                    "Is navigation in progress: ${isBlocked.value}"
                 )
 
+                if (navController.currentBackStackEntry!!.destination.route == screen.route || isBlocked.value) return@IconButton
+
+                navController.navigate(screen.route).also {
+                    Log.d(
+                        "Navigation",
+                        "Updated isBlocked to true"
+                    )
+
+                    isBlocked.value = true
+                }
                 return@IconButton
             } ?: run {
                 Log.d(
@@ -36,7 +63,7 @@ fun NavigationItem(
                 )
 
                 //Starting navigation and current navigation
-                if(navController.currentBackStack.value.size == 2) return@IconButton
+                if (navController.currentBackStack.value.size == 2) return@IconButton
 
                 navController.popBackStack()
 
@@ -52,5 +79,17 @@ fun NavigationItem(
             is Painter -> Icon(icon, contentDescription = label)
             else -> return@IconButton
         }
+    }
+}
+
+@OptIn(DelicateCoroutinesApi::class)
+fun blockNavigationButtons(isBlocked: MutableState<Boolean>) {
+    GlobalScope.launch {
+        delay(750)
+        Log.d(
+            "Navigation",
+            "Updated isBlocked to false"
+        )
+        isBlocked.value = false
     }
 }
