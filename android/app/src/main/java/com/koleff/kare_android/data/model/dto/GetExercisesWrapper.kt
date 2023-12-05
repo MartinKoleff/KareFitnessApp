840d60df835058a73dcb314c4d1ff0737dcd981b
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-05 12:24:59 +0200

    Added server response data wrapper for getExercises.

---
+++ android/app/src/main/java/com/koleff/kare_android/data/model/dto/GetExercisesWrapper.kt
@@ -0,0 +1,10 @@
+package com.koleff.kare_android.data.model.dto
+
+import com.koleff.kare_android.data.model.response.GetExercisesResponse
+import com.koleff.kare_android.data.model.response.base_response.ServerResponseData
+
+class GetExercisesWrapper(getExercisesResponse: GetExercisesResponse) :
+    ServerResponseData(getExercisesResponse) {
+    val exercisesList = getExercisesResponse.exercisesList
+//    val paginationData = getExercisesResponse.paginationData
+}
