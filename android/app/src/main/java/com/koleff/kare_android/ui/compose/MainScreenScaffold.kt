c0e0cab5df85e00e5570271312bcc9131b234b16
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-05 14:03:34 +0200

    Passing isNavigationInProgress to Toolbar, BottomNavigationBar and MainScreenScaffold.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/MainScreenScaffold.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/MainScreenScaffold.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.koleff.kare_android.ui.compose.navigation.BottomNavigationBar
@@ -13,15 +14,16 @@ import com.koleff.kare_android.ui.compose.navigation.Toolbar
 fun MainScreenScaffold(
     screenTitle: String,
     navController: NavHostController,
+    isNavigationInProgress: MutableState<Boolean>,
     modifierPadding: @Composable (paddingValues: PaddingValues) -> Unit
 ) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            Toolbar(navController = navController, title = screenTitle)
+            Toolbar(navController = navController, title = screenTitle, isNavigationInProgress = isNavigationInProgress)
         },
         bottomBar = {
-            BottomNavigationBar(navController = navController)
+            BottomNavigationBar(navController = navController, isNavigationInProgress = isNavigationInProgress)
         }
     ) { innerPadding ->
         modifierPadding(innerPadding)
