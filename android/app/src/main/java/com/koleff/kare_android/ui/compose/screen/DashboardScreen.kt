833610f650d6657fe8eda062dbe7b81f0248d6e3
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-05 17:11:29 +0200

    Wired Dashboard with MuscleGroupGrid.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/screen/DashboardScreen.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/screen/DashboardScreen.kt
@@ -4,8 +4,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.navigation.NavHostController
+import com.koleff.kare_android.R
+import com.koleff.kare_android.common.DataManager
+import com.koleff.kare_android.data.MainScreen
+import com.koleff.kare_android.data.model.dto.MuscleGroup
 import com.koleff.kare_android.ui.compose.MainScreenScaffold
+import com.koleff.kare_android.ui.compose.MuscleGroupGrid
 
 @Composable
 fun DashboardScreen(navController: NavHostController) {
@@ -13,5 +19,11 @@ fun DashboardScreen(navController: NavHostController) {
         val modifier = Modifier
             .padding(innerPadding)
             .fillMaxSize()
+
+        MuscleGroupGrid(
+            modifier = modifier,
+            navController = navController,
+            muscleGroupList = DataManager.muscleGroupList
+        )
     }
 }
