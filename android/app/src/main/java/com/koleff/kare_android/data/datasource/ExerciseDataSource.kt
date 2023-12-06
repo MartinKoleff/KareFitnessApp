31c61f1beecea64557d7636c205f6cdd2974967a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-06 14:19:25 +0200

    Updated ExerciseDataSource. Wired RemoteDataSource with new changes.

--- android/app/src/main/java/com/koleff/kare_android/data/datasource/ExerciseDataSource.kt
+++ android/app/src/main/java/com/koleff/kare_android/data/datasource/ExerciseDataSource.kt
@@ -1,9 +1,11 @@
 package com.koleff.kare_android.data.datasource
 
-import com.koleff.kare_android.data.model.dto.GetExercisesWrapper
+import com.koleff.kare_android.data.model.wrapper.GetExerciseDetailsWrapper
+import com.koleff.kare_android.data.model.wrapper.GetExercisesWrapper
 import com.koleff.kare_android.data.model.wrapper.ResultWrapper
 import kotlinx.coroutines.flow.Flow
 
 interface ExerciseDataSource {
-    suspend fun getExercises(): Flow<ResultWrapper<GetExercisesWrapper>>
+    suspend fun getExercises(muscleGroupId: Int): Flow<ResultWrapper<GetExercisesWrapper>>
+    suspend fun getExerciseDetails(exerciseId: Int): Flow<ResultWrapper<GetExerciseDetailsWrapper>>
 }
--- android/app/src/main/java/com/koleff/kare_android/data/datasource/ExerciseRemoteDataSource.kt
+++ android/app/src/main/java/com/koleff/kare_android/data/datasource/ExerciseRemoteDataSource.kt
@@ -1,9 +1,10 @@
 package com.koleff.kare_android.data.datasource
 
 import com.koleff.kare_android.common.Network
-import com.koleff.kare_android.data.model.dto.GetExercisesWrapper
+import com.koleff.kare_android.data.model.wrapper.GetExercisesWrapper
+import com.koleff.kare_android.data.model.request.GetExerciseDetailsRequest
 import com.koleff.kare_android.data.model.request.GetExercisesRequest
-import com.koleff.kare_android.data.model.response.base_response.ServerResponseData
+import com.koleff.kare_android.data.model.wrapper.GetExerciseDetailsWrapper
 import com.koleff.kare_android.data.model.wrapper.ResultWrapper
 import com.koleff.kare_android.data.remote.ExerciseApi
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,9 +19,15 @@ class ExerciseRemoteDataSource @Inject constructor(
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ExerciseDataSource {
 
-    override suspend fun getExercises(): Flow<ResultWrapper<GetExercisesWrapper>> {
-        val body = GetExercisesRequest() //TODO: fill with data...
+    override suspend fun getExercises(muscleGroupId: Int): Flow<ResultWrapper<GetExercisesWrapper>> {
+        val body = GetExercisesRequest(muscleGroupId)
 
-        return Network.executeApiCall(dispatcher, { GetExercisesWrapper(exerciseApi.getExercises()) })
+        return Network.executeApiCall(dispatcher, { GetExercisesWrapper(exerciseApi.getExercises(body)) })
+    }
+
+    override suspend fun getExerciseDetails(exerciseId: Int): Flow<ResultWrapper<GetExerciseDetailsWrapper>> {
+        val body = GetExerciseDetailsRequest(exerciseId)
+
+        return Network.executeApiCall(dispatcher, { GetExerciseDetailsWrapper(exerciseApi.getExerciseDetails(body)) })
     }
 }
