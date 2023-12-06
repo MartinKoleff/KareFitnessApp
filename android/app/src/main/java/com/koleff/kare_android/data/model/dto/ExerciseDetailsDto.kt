9b305d2f38db7e9d8b208d604ee3be392cbd6668
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-06 14:20:19 +0200

    Added Exercise DTOs and enums.

--- android/app/src/main/java/com/koleff/kare_android/data/model/dto/ExerciseDetailsDto.kt
+++ android/app/src/main/java/com/koleff/kare_android/data/model/dto/ExerciseDetailsDto.kt
@@ -2,13 +2,15 @@ package com.koleff.kare_android.data.model.dto
 
 import com.squareup.moshi.Json
 
-data class ExerciseDto(
+data class ExerciseDetailsDto(
     @field:Json(name = "name")
     val name: String,
+    @field:Json(name = "description")
+    val description: String,
     @field:Json(name = "muscle_group")
     val muscleGroup: MuscleGroup,
     @field:Json(name = "machine")
     val machineType: MachineType,
-    @field:Json(name = "snapshot") //TODO: image...
-    val snapshot: String,
+    @field:Json(name = "video_url")
+    val videoUrl: String
 )
