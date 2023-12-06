22ceba7250920cd67625574608c6239ebfe02c43
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-06 15:20:45 +0200

    Added all workout response wrappers.

--- android/app/src/main/java/com/koleff/kare_android/data/model/wrapper/GetExercisesWrapper.kt
+++ android/app/src/main/java/com/koleff/kare_android/data/model/wrapper/GetExercisesWrapper.kt
@@ -4,6 +4,6 @@ import com.koleff.kare_android.data.model.response.GetExercisesResponse
 
 class GetExercisesWrapper(getExercisesResponse: GetExercisesResponse) :
     ServerResponseData(getExercisesResponse) {
-    val exercisesList = getExercisesResponse.exercisesList
+    val exercises = getExercisesResponse.exercises
 //    val paginationData = getExercisesResponse.paginationData
 }
