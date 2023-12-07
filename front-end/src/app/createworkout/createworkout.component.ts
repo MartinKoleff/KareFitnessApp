b589a21ad3e13b6e3f7ba641f6433b263f8e9f99
Chavdar <cavdar2002@gmail.com>
2023-12-07 03:39:48 +0200

    Added exercises

--- front-end/src/app/createworkout/createworkout.component.ts
+++ front-end/src/app/createworkout/createworkout.component.ts
@@ -6,6 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./createworkout.component.css']
 })
 export class CreateworkoutComponent {
+  showTricepsExercise: boolean = false;
+  showBicepsExercise: boolean = false;
+  showLegsExercise: boolean = false;
+  showBackExercise: boolean = false;
+  showShouldersExercise: boolean = false;
+  showChestExercise: boolean = false;
+  showCardioExercise: boolean = false;
+  showAbsExercise: boolean = false;
+
+  toggleBicepsExercise() {
+    this.showBicepsExercise = !this.showBicepsExercise;
+  }
+  toggleTricepsExercise() {
+    this.showTricepsExercise = !this.showTricepsExercise;
+  }
+  toggleLegsExercise() {
+    this.showTricepsExercise = !this.showTricepsExercise;
+  }
+  toggleBackExercise() {
+    this.showBackExercise = !this.showBackExercise;
+  }
+  toggleShouldersExercise() {
+    this.showShouldersExercise = !this.showShouldersExercise;
+  }
+  toggleChestExercise() {
+    this.showChestExercise = !this.showChestExercise;
+  }
+  toggleCardioExercise() {
+    this.showCardioExercise = !this.showCardioExercise;
+  }
+  toggleAbsExercise() {
+    this.showAbsExercise = !this.showAbsExercise;
+  }
   submitWorkout(){
 
   }
