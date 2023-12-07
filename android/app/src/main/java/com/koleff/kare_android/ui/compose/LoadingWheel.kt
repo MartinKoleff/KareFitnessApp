18fb059c4e2999596d87b9737c619ee50a66f780
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-07 23:07:43 +0200

    Added loading wheel composable.

---
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/LoadingWheel.kt
@@ -0,0 +1,21 @@
+package com.koleff.kare_android.ui.compose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingWheel(modifier: Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(modifier = Modifier
+            .size(50.dp)
+        )
+    }
+}
