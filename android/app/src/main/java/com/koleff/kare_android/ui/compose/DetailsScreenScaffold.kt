d774378dc827bc77202913baa8234f552df1c429
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-10 15:46:24 +0200

    Updated DetailsScreenScaffold toolbar to ExerciseDetailsToolbar.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/DetailsScreenScaffold.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/DetailsScreenScaffold.kt
@@ -1,12 +1,18 @@
 package com.koleff.kare_android.ui.compose
 
+import ExerciseDetailsToolbar
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.navigation.NavHostController
+import com.koleff.kare_android.R
 import com.koleff.kare_android.ui.compose.navigation.BottomNavigationBar
 import com.koleff.kare_android.ui.compose.navigation.Toolbar
 
@@ -15,12 +21,30 @@ fun DetailsScreenScaffold(
     screenTitle: String,
     navController: NavHostController,
     isNavigationInProgress: MutableState<Boolean>,
+    exerciseImageId: Int,
     modifierPadding: @Composable (paddingValues: PaddingValues) -> Unit
 ) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            Toolbar(navController = navController, title = screenTitle, isNavigationInProgress = isNavigationInProgress)
+            ExerciseDetailsToolbar(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                MaterialTheme.colorScheme.primaryContainer,
+                                MaterialTheme.colorScheme.primaryContainer,
+                                MaterialTheme.colorScheme.primaryContainer,
+                                MaterialTheme.colorScheme.primary,
+                                MaterialTheme.colorScheme.primary
+                            )
+                        )
+                    ),
+                isNavigationInProgress = isNavigationInProgress,
+                navController = navController,
+                exerciseImageId = exerciseImageId
+            )
         },
     ) { innerPadding ->
         modifierPadding(innerPadding)
