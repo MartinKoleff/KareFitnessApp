ffc72839324e9de205e030be6d44c81cc791e6de
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-11 00:46:40 +0200

    Added Workout details screen route.

--- android/app/src/main/java/com/koleff/kare_android/data/MainScreen.kt
+++ android/app/src/main/java/com/koleff/kare_android/data/MainScreen.kt
@@ -10,6 +10,10 @@ sealed class MainScreen(val route: String) {
     object ExerciseDetails : MainScreen("exercise_details/{exercise_id}/{muscle_group_id}"){ //When exercise is selected -> show details screen
         fun createRoute(exerciseId: Int, muscleGroupId: Int) = "exercise_details/$exerciseId/$muscleGroupId"
     }
+
+    object WorkoutDetails : MainScreen("workout_details/{workout_id}"){ //When workout is selected -> show details screen
+        fun createRoute(workoutId: String) = "workout_details/$workoutId/"
+    }
     object Settings : MainScreen("settings") //Settings list
     object SearchWorkoutsScreen : MainScreen("search_workouts") //Select workout where exercise will be added
 }
