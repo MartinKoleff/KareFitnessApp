d6fdc0b496b82f330560fe0dbef51b5027c606f4
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-11 14:20:35 +0200

    Added Workout Entity.

---
+++ android/app/src/main/java/com/koleff/kare_android/data/room/dto/Workout.kt
@@ -0,0 +1,17 @@
+package com.koleff.kare_android.data.room.dto
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.koleff.kare_android.data.model.dto.MuscleGroup
+import com.squareup.moshi.Json
+
+@Entity
+data class Workout(
+    @PrimaryKey(autoGenerate = true)
+    val workoutId: String,
+    val name: String,
+    val muscleGroup: MuscleGroup,
+    val snapshot: String,
+    val totalExercises: Int,
+    val isSelected: Boolean,
+)
