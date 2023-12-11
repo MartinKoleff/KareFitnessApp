3814ccf1b11cb3843b8876ac05fd805cf9938542
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-11 22:20:16 +0200

    Wired ExerciseDetailsDao with ExerciseLocalDataSource.

--- android/app/src/main/java/com/koleff/kare_android/data/datasource/ExerciseLocalDataSource.kt
+++ android/app/src/main/java/com/koleff/kare_android/data/datasource/ExerciseLocalDataSource.kt
@@ -9,6 +9,7 @@ import com.koleff.kare_android.data.model.wrapper.GetExerciseDetailsWrapper
 import com.koleff.kare_android.data.model.wrapper.GetExercisesWrapper
 import com.koleff.kare_android.data.model.wrapper.ResultWrapper
 import com.koleff.kare_android.data.room.dao.ExerciseDao
+import com.koleff.kare_android.data.room.dao.ExerciseDetailsDao
 import com.koleff.kare_android.data.room.entity.Exercise
 import com.koleff.kare_android.data.room.manager.ExerciseDBManager
 import kotlinx.coroutines.delay
@@ -18,6 +19,7 @@ import javax.inject.Inject
 
 class ExerciseLocalDataSource @Inject constructor(
     private val exerciseDao: ExerciseDao,
+    private val exerciseDetailsDao: ExerciseDetailsDao,
     private val exerciseDBManager: ExerciseDBManager
 ) : ExerciseDataSource {
 
@@ -28,7 +30,7 @@ class ExerciseLocalDataSource @Inject constructor(
 
             //Check if Room DB has data
             if (!exerciseDBManager.hasInitializedExerciseTableRoomDB) {
-                exerciseDBManager.initializeExerciseTableRoomDB(exerciseDao)
+                exerciseDBManager.initializeExerciseTableRoomDB(exerciseDao, exerciseDetailsDao)
             }
 
             val data = exerciseDao.getExercisesOrderedById(
@@ -49,7 +51,8 @@ class ExerciseLocalDataSource @Inject constructor(
             emit(ResultWrapper.Loading())
             delay(Constants.fakeDelay)
 
-            val data = exerciseDao.getExerciseById(exerciseId)
+            //Temporary setting the same description and videoUrl to all ExerciseDetails entities.
+            val data = exerciseDetailsDao.getExerciseDetailsById(exerciseId)
             val description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum nibh nec pharetra iaculis. Aenean ultricies egestas leo at ultricies. Quisque suscipit, purus ut congue porta, eros eros tincidunt sem, sed commodo magna metus eu nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum quis velit eget eros malesuada luctus. Suspendisse iaculis ullamcorper condimentum. Sed metus augue, dapibus eu venenatis vitae, ornare non turpis. Donec suscipit iaculis dolor, id fermentum mauris interdum in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
             val videoUrl = "dQw4w9WgXcQ" //https://www.youtube.com/watch?v=
 
