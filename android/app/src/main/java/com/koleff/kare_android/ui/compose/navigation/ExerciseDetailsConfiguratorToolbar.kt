97ff4da37e8e099ed7f9b90d6ba68550b78646a0
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-12 13:46:54 +0200

    Added ExerciseDetailsConfigurator toolbar.
    The toolbar consists of Submit and Back buttons.

---
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/navigation/ExerciseDetailsConfiguratorToolbar.kt
@@ -0,0 +1,136 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.koleff.kare_android.R
+import com.koleff.kare_android.data.MainScreen
+import com.koleff.kare_android.data.model.dto.ExerciseDto
+import com.koleff.kare_android.data.model.event.OnWorkoutDetailsEvent
+import com.koleff.kare_android.ui.compose.navigation.NavigationItem
+import com.koleff.kare_android.ui.view_model.WorkoutDetailsViewModel
+
+
+@Composable
+fun ExerciseDetailsConfiguratorToolbar(
+    modifier: Modifier,
+    navController: NavHostController,
+    isNavigationInProgress: MutableState<Boolean>,
+    exerciseImageId: Int
+) {
+    val primaryColor = MaterialTheme.colorScheme.primary
+    val primaryContainerColor = MaterialTheme.colorScheme.primaryContainer
+
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+
+    Box(
+        modifier = modifier
+    ) {
+
+        //Exercise Muscle Group Image
+        Image(
+            painter = painterResource(id = exerciseImageId),
+            contentDescription = "Image",
+            contentScale = ContentScale.Crop,
+            modifier = modifier.clip(RoundedToolbarShape())
+        )
+
+        //Navigation
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .drawBehind {
+
+                },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            NavigationItem(
+                navController = navController,
+                screen = null, //Backstack pop
+                icon = Icons.Filled.ArrowBack,
+                label = "Go back",
+                isBlocked = isNavigationInProgress,
+                tint = Color.White
+            )
+
+            //Add onClick to add to view model...
+            NavigationItem(
+                navController = navController,
+                screen = MainScreen.WorkoutDetails,
+                icon = painterResource(id = R.drawable.ic_vector_select),
+                label = "Submit exercise",
+                isBlocked = isNavigationInProgress,
+                tint = Color.Green
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewExerciseConfiguratorToolbar() {
+    val navController = rememberNavController()
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+
+    ExerciseDetailsConfiguratorToolbar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(screenHeight / 2.5f)
+            .background(
+//                brush = Brush.verticalGradient(
+//                    colors = listOf(
+//                        MaterialTheme.colorScheme.primaryContainer,
+//                        MaterialTheme.colorScheme.primaryContainer,
+//                        MaterialTheme.colorScheme.primaryContainer,
+//                        MaterialTheme.colorScheme.primary,
+//                        MaterialTheme.colorScheme.primary
+//                    )
+//                )
+                color = MaterialTheme.colorScheme.primaryContainer
+            ),
+        isNavigationInProgress = mutableStateOf(false),
+        navController = navController,
+        exerciseImageId = R.drawable.ic_legs
+    )
+}
+
+
+
+
