886cf4aaf2fdea1ebcec7dba8a919286add5d99a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-13 17:56:50 +0200

    Moved ExerciseDetails bottom navigation bar to new file. Added previews for toolbar and bottom navigation bars.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/navigation/ExerciseDetailsBottomNavigationVar.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/navigation/ExerciseDetailsBottomNavigationVar.kt
@@ -1,15 +1,11 @@
 package com.koleff.kare_android.ui.compose.navigation
 
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -20,44 +16,37 @@ import androidx.navigation.compose.rememberNavController
 import com.koleff.kare_android.R
 import com.koleff.kare_android.data.MainScreen
 
+
 @Composable
-fun BottomNavigationBar(
+fun ExerciseDetailsBottomNavigationBar(
     navController: NavHostController,
     isNavigationInProgress: MutableState<Boolean>
 ) {
     BottomAppBar(
+        containerColor = MaterialTheme.colorScheme.secondary,
+        tonalElevation = 5.dp,
         actions = {
-            NavigationItem(
+            Spacer(Modifier.weight(1f))
+
+            FloatingNavigationItem(
                 navController = navController,
-                screen = MainScreen.Dashboard,
-                icon = Icons.Filled.Star,
-                label = "Dashboard",
-                isBlocked = isNavigationInProgress
+                screen = MainScreen.SearchWorkoutsScreen,
+                icon = painterResource(id = R.drawable.ic_vector_add),
+                label = "Add to workout",
+                isBlocked = isNavigationInProgress,
+                tint = Color.White
             )
 
-            //Spacing between items
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-                    .width(0.dp)
-            )
-
-            NavigationItem(
-                navController = navController,
-                screen = MainScreen.MyWorkout,
-                icon = painterResource(id = R.drawable.ic_vector_my_workout),
-                label = "Workout screen",
-                isBlocked = isNavigationInProgress
-            )
+            Spacer(Modifier.weight(1f))
         }
     )
 }
 
 @Preview
 @Composable
-fun PreviewBottomNavigationBar() {
+fun PreviewExerciseDetailsBottomNavigationBar() {
     val navController = rememberNavController()
-        BottomNavigationBar(
+    ExerciseDetailsBottomNavigationBar(
         navController = navController,
         isNavigationInProgress = mutableStateOf(false)
     )
