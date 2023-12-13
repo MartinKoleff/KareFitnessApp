ae45830cde9b21d71fb6f1d2fd3988c458ca4b99
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-13 17:20:47 +0200

    Added ExerciseBanner list in WorkoutDetailsScreen.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/screen/WorkoutDetailsScreen.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/screen/WorkoutDetailsScreen.kt
@@ -1,55 +1,28 @@
 package com.koleff.kare_android.ui.compose.screen
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.koleff.kare_android.R
 import com.koleff.kare_android.data.MainScreen
-import com.koleff.kare_android.data.model.dto.ExerciseDetailsDto
-import com.koleff.kare_android.data.model.dto.ExerciseDto
-import com.koleff.kare_android.data.model.dto.MachineType
-import com.koleff.kare_android.data.model.dto.MuscleGroup
-import com.koleff.kare_android.data.model.state.ExerciseDetailsState
-import com.koleff.kare_android.ui.compose.scaffolds.ExerciseDetailsScreenScaffold
 import com.koleff.kare_android.ui.compose.LoadingWheel
-import com.koleff.kare_android.ui.compose.YoutubeVideoPlayer
 import com.koleff.kare_android.ui.compose.banners.AddExerciseToWorkoutBanner
-import com.koleff.kare_android.ui.compose.navigation.FloatingNavigationItem
+import com.koleff.kare_android.ui.compose.banners.ExerciseBannerV2
 import com.koleff.kare_android.ui.compose.scaffolds.MainScreenScaffold
-import com.koleff.kare_android.ui.view_model.ExerciseDetailsViewModel
 import com.koleff.kare_android.ui.view_model.WorkoutDetailsViewModel
 
 @Composable
@@ -60,8 +33,9 @@ fun WorkoutDetailsScreen(
     workoutDetailsViewModel: WorkoutDetailsViewModel
 ) {
     val workoutDetailsState by workoutDetailsViewModel.state.collectAsState()
-    val workoutTitle = workoutDetailsState.workout.name
-    val totalExercises = workoutDetailsState.workout.exercises.size
+    val workoutTitle =
+        if (workoutDetailsState.workout.name == "") "Loading..." else workoutDetailsState.workout.name
+    val exercises = workoutDetailsState.workout.exercises
 
     MainScreenScaffold(workoutTitle, navController, isNavigationInProgress) { innerPadding ->
         val modifier = Modifier
@@ -83,8 +57,22 @@ fun WorkoutDetailsScreen(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                items(totalExercises) {
-                    //Exercises banners...
+                items(exercises.size) { currentExerciseId ->
+                    val currentExercise = exercises[currentExerciseId]
+                    ExerciseBannerV2(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp),
+                        exercise = currentExercise,
+                    ) { selectedExercise ->
+                        navController.navigate(
+                            MainScreen.ExerciseDetailsConfigurator.createRoute(
+                                exerciseId = selectedExercise.exerciseId,
+                                workoutId = workoutId,
+                                muscleGroupId = selectedExercise.muscleGroup.muscleGroupId
+                            )
+                        )
+                    }
                 }
 
                 item {
