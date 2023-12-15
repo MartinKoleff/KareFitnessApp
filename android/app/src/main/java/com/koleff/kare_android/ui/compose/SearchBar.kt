d2cb1f5c3892f20c23bb6198fd632b755c61a709
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-15 15:35:19 +0200

    Fixed indicator line in SearchBar.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/SearchBar.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/SearchBar.kt
@@ -2,20 +2,24 @@ package com.koleff.kare_android.ui.compose
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
@@ -23,6 +27,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier,
@@ -36,7 +41,13 @@ fun SearchBar(
     Row(modifier = modifier) {
         TextField(
             value = text,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent, //Hide the bottom line indicator
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
             onValueChange = {
                 if ((text.isNotEmpty() && it.isEmpty()) ||
                     (text.isEmpty() && it.isNotEmpty())
@@ -51,12 +62,10 @@ fun SearchBar(
             leadingIcon = {
                 Icon(Icons.Filled.Search, contentDescription = "Search icon")
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text, imeAction = ImeAction.Search),
-//        onImeActionPerformed = {
-//            if (imeAction == ImeAction.Done) {
-//                hideKeyboard(context as Activity)
-//            }
-//        },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
             keyboardActions = KeyboardActions(
                 onSearch = {
                     onSearch(text)
