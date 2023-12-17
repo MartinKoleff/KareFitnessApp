2859e41a6aa633909dfbe5fe8e73064b6a36c847
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-17 23:58:29 +0200

    Updated LoadingWheel. Changed color to primary purple.

--- android/app/src/main/java/com/koleff/kare_android/ui/compose/LoadingWheel.kt
+++ android/app/src/main/java/com/koleff/kare_android/ui/compose/LoadingWheel.kt
@@ -1,5 +1,6 @@
 package com.koleff.kare_android.ui.compose
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +18,9 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoadingWheel(innerPadding: PaddingValues = PaddingValues(0.dp)) {
+fun LoadingWheel(innerPadding: PaddingValues = PaddingValues(0.dp), hideScreen: Boolean = false) {
+    val background = if (hideScreen) Color.White else Color.Transparent
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -25,13 +29,14 @@ fun LoadingWheel(innerPadding: PaddingValues = PaddingValues(0.dp)) {
                 start = 8.dp + innerPadding.calculateStartPadding(LayoutDirection.Rtl),
                 end = 8.dp + innerPadding.calculateEndPadding(LayoutDirection.Rtl),
                 bottom = 8.dp + innerPadding.calculateBottomPadding()
-            ),
+            )
+            .background(background),
         contentAlignment = Alignment.Center
     ) {
         CircularProgressIndicator(
             modifier = Modifier
                 .size(50.dp),
-            color = Color.Green
+            color = MaterialTheme.colorScheme.primary
         )
     }
 }
