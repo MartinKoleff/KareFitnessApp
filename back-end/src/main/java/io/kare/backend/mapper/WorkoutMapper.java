7b0154eedf67771b96dea8ab4cc10b9d76952b68
GabrielGenkov <gabriel.genkov2002@gmail.com>
2024-01-04 19:46:11 +0200

    Communication made possible

--- back-end/src/main/java/io/kare/backend/mapper/WorkoutMapper.java
+++ back-end/src/main/java/io/kare/backend/mapper/WorkoutMapper.java
@@ -11,99 +11,124 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface WorkoutMapper {
 
-    @Mapping(target = "user", source = "user")
-    @Mapping(target = "exercises", source = "exercises")
-    @Mapping(target = "programs", source = "programs")
-    WorkoutEntity mapToEntity(AddWorkoutRequest request, UserEntity user, List<ExerciseEntity> exercises, List<ProgramEntity> programs);
+	@Mapping(target = "user", source = "user")
+	@Mapping(target = "exercises", source = "exercises")
+	@Mapping(target = "programs", source = "programs")
+	WorkoutEntity mapToEntity(
+		AddWorkoutRequest request,
+		UserEntity user,
+		List<ExerciseEntity> exercises,
+		List<ProgramEntity> programs
+	);
 
-    @Mapping(target = "user", source = "user")
-    @Mapping(target = "exercises", source = "exercises")
-    @Mapping(target = "programs", source = "programs")
-    WorkoutEntity mapToEntity(AddFullWorkoutRequest request, UserEntity user, List<ExerciseEntity> exercises, List<ProgramEntity> programs);
+	@Mapping(target = "user", source = "user")
+	@Mapping(target = "exercises", source = "exercises")
+	@Mapping(target = "programs", source = "programs")
+	WorkoutEntity mapToEntity(
+		AddFullWorkoutRequest request,
+		UserEntity user,
+		List<ExerciseEntity> exercises,
+		List<ProgramEntity> programs
+	);
 
-    default WorkoutEntity mapToEntity(AddWorkoutRequest request, UserEntity user, List<ExerciseEntity> exercises) {
-        return this.mapToEntity(request, user, exercises, new ArrayList<>());
-    }
+	default WorkoutEntity mapToEntity(
+		AddWorkoutRequest request,
+		UserEntity user,
+		List<ExerciseEntity> exercises
+	) {
+		return this.mapToEntity(request, user, exercises, new ArrayList<>());
+	}
 
-    default WorkoutEntity mapToEntity(AddFullWorkoutRequest request, UserEntity user, List<ExerciseEntity> exercises) {
-        return this.mapToEntity(request, user, exercises, new ArrayList<>());
-    }
+	default WorkoutEntity mapToEntity(
+		AddFullWorkoutRequest request,
+		UserEntity user,
+		List<ExerciseEntity> exercises
+	) {
+		return this.mapToEntity(request, user, exercises, new ArrayList<>());
+	}
 
-    AddWorkoutResponse mapToResponse(WorkoutEntity workoutEntity);
+	AddWorkoutResponse mapToResponse(WorkoutEntity workoutEntity);
 
-    default GetWorkoutsResponse mapToGetWorkouts(
-        List<WorkoutEntity> workouts,
-        List<ExerciseOptionEntity> exerciseOptions
-    ) {
-        return new GetWorkoutsResponse(
-            workouts.stream()
-                .map(workout -> new GetWorkoutResponse(
-                    workout.getId(),
-                    workout.getName(),
-                    workout.getDescription(),
-                    this.mapToGetExercises(
-                        workout.getExercises(),
-                        this.findExerciseOptionsByWorkoutId(workout.getId(), exerciseOptions)
-                    )
-                ))
-                .toList()
-        );
-    }
+	default GetWorkoutsResponse mapToGetWorkouts(
+		List<WorkoutEntity> workouts,
+		List<ExerciseOptionEntity> exerciseOptions
+	) {
+		return new GetWorkoutsResponse(
+			workouts.stream()
+				.map(workout -> {
+					var exercises = this.mapToGetExercises(
+						workout.getExercises(),
+						this.findExerciseOptionsByWorkoutId(workout.getId(), exerciseOptions)
+					);
+					return new GetWorkoutResponse(
+						workout.getId(),
+						workout.getName(),
+						workout.getDescription(),
+						exercises,
+						exercises.size()
+					);
+				})
+				.toList()
+		);
+	}
 
-    default GetWorkoutResponse mapToGetWorkout(
-        WorkoutEntity workoutEntity,
-        List<ExerciseOptionEntity> exerciseOptions
-    ) {
-        return new GetWorkoutResponse(
-            workoutEntity.getId(),
-            workoutEntity.getName(),
-            workoutEntity.getDescription(),
-                    this.mapToGetExercises(
-                        workoutEntity.getExercises(),
-                        this.findExerciseOptionsByWorkoutId(workoutEntity.getId(), exerciseOptions)
-                    ));
-    }
+	default GetWorkoutResponse mapToGetWorkout(
+		WorkoutEntity workoutEntity,
+		List<ExerciseOptionEntity> exerciseOptions
+	) {
+		var exercises = this.mapToGetExercises(
+			workoutEntity.getExercises(),
+			this.findExerciseOptionsByWorkoutId(workoutEntity.getId(), exerciseOptions)
+		);
+		return new GetWorkoutResponse(
+			workoutEntity.getId(),
+			workoutEntity.getName(),
+			workoutEntity.getDescription(),
+			exercises,
+			exercises.size()
+		);
+	}
 
-    private List<WorkoutExerciseData> mapToGetExercises(
-        List<ExerciseEntity> exercises,
-        List<ExerciseOptionEntity> exerciseOptions
-    ) {
-        return exercises.stream()
-            .map(exercise -> {
-                ExerciseOptionEntity exerciseOption = exerciseOptions.stream()
-                    .filter(option -> option.getExercise()
-                        .getId()
-                        .equals(exercise.getId()))
-                    .findFirst()
-                    .orElse(null);
-                if (exerciseOption == null) {
-                    return null;
-                }
-                return new WorkoutExerciseData(
-                    new ExercisePayload(
-                        exercise.getId(),
-                        exercise.getName(),
-                        exercise.getDescription(),
-                        exercise.getMuscleGroup(),
-                        exercise.getMachineType(),
-                        exercise.getUrl()
-                    ),
-                    exerciseOption.getSets(),
-                    exerciseOption.getReps(),
-                    exerciseOption.getWeight()
-                );
-            })
-            .toList();
-    }
+	private List<WorkoutExerciseData> mapToGetExercises(
+		List<ExerciseEntity> exercises,
+		List<ExerciseOptionEntity> exerciseOptions
+	) {
+		return exercises.stream()
+			.map(exercise -> {
+				ExerciseOptionEntity exerciseOption = exerciseOptions.stream()
+					.filter(option -> option.getExercise()
+						.getId()
+						.equals(exercise.getId()))
+					.findFirst()
+					.orElse(null);
+				if (exerciseOption == null) {
+					return null;
+				}
+				return new WorkoutExerciseData(
+					new ExercisePayload(
+						exercise.getId(),
+						exercise.getName(),
+						exercise.getDescription(),
+						exercise.getMuscleGroup(),
+						exercise.getMachineType(),
+						exercise.getUrl()
+					),
+					exerciseOption.getSets(),
+					exerciseOption.getReps(),
+					exerciseOption.getWeight()
+				);
+			})
+			.toList();
+	}
 
-    private List<ExerciseOptionEntity> findExerciseOptionsByWorkoutId(
-        String workoutId,
-        List<ExerciseOptionEntity> exerciseOptions
-    ) {
-        return exerciseOptions.stream()
-            .filter(exerciseOption -> exerciseOption.getWorkout()
-                .getId()
-                .equals(workoutId))
-            .toList();
-    }
+	private List<ExerciseOptionEntity> findExerciseOptionsByWorkoutId(
+		String workoutId,
+		List<ExerciseOptionEntity> exerciseOptions
+	) {
+		return exerciseOptions.stream()
+			.filter(exerciseOption -> exerciseOption.getWorkout()
+				.getId()
+				.equals(workoutId))
+			.toList();
+	}
 }
--- back-end/src/main/java/io/kare/backend/payload/response/GetWorkoutResponse.java
+++ back-end/src/main/java/io/kare/backend/payload/response/GetWorkoutResponse.java
@@ -4,5 +4,5 @@ import io.kare.backend.payload.data.*;
 
 import java.util.List;
 
-public record GetWorkoutResponse(String id, String name, String description, List<WorkoutExerciseData> exercises) {
+public record GetWorkoutResponse(String id, String name, String description, List<WorkoutExerciseData> exercises, int totalExercises) {
 }
