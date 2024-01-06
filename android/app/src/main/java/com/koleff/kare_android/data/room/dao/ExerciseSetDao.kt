fd15c09ce1025c0c6fb9fd37678919e3cc637ab4
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2024-01-06 21:24:09 +0200

    Added ExerciseSetDao. Updated KareDB to v3.

---
+++ android/app/src/main/java/com/koleff/kare_android/data/room/dao/ExerciseSetDao.kt
@@ -0,0 +1,29 @@
+package com.koleff.kare_android.data.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import com.koleff.kare_android.data.model.dto.MuscleGroup
+import com.koleff.kare_android.data.room.entity.Exercise
+import com.koleff.kare_android.data.room.entity.ExerciseDetails
+import com.koleff.kare_android.data.room.entity.SetEntity
+import com.koleff.kare_android.data.room.entity.relations.ExerciseDetailsExerciseCrossRef
+import com.koleff.kare_android.data.room.entity.relations.ExerciseSetCrossRef
+import com.koleff.kare_android.data.room.entity.relations.ExerciseWithSet
+
+@Dao
+interface ExerciseSetDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveSet(setEntity: SetEntity)
+
+    @Update
+    suspend fun updateSet(setEntity: SetEntity)
+
+    @Delete
+    suspend fun deleteSet(setEntity: SetEntity)
+}
