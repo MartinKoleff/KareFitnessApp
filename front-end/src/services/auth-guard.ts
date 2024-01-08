b407d10eb9b18b16f3396e1df56649311cf38fa8
Chavdar <cavdar2002@gmail.com>
2024-01-08 16:07:34 +0200

    removed unused dependency AuthGuard

--- front-end/src/services/auth-guard.ts
+++ front-end/src/services/auth-guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { AuthService } from './auth-service';
 import { CookieService } from 'ngx-cookie';
 
 @Injectable({
@@ -8,7 +7,6 @@ import { CookieService } from 'ngx-cookie';
 })
 export class AuthGuard implements CanActivate {
   constructor(
-    private authService: AuthService,
     private router: Router,
     private cookieService: CookieService
   ) {}
