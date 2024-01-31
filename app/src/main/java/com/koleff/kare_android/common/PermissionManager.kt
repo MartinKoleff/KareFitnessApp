bc840958818f7a7e59d51f9e86d9be273b306b58
MartinKoleff <martin_koleff@abv.bg>
2024-01-31 18:20:36 +0200

    Added hasNotification permission. Added < API 33 custom prompt logic for requestNotificationPermission.

--- app/src/main/java/com/koleff/kare_android/common/PermissionManager.kt
+++ app/src/main/java/com/koleff/kare_android/common/PermissionManager.kt
@@ -4,32 +4,62 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
-import android.hardware.biometrics.BiometricManager
 import android.os.Build
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
-import androidx.core.hardware.fingerprint.FingerprintManagerCompat
+
 
 object PermissionManager {
 
-    fun requestNotificationPermission(context: Context){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            val activity = context as? Activity ?: return  //Return if the context is not an instance of Activity
+    fun requestNotificationPermission(context: Context): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val activity = context as? Activity
+                ?: return false //Return if the context is not an instance of Activity
 
             val hasPermission = ContextCompat.checkSelfPermission(
                 activity,
-                Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
 
             //Prompt user to enable push notifications
-            if(!hasPermission){
+            if (!hasPermission) {
                 ActivityCompat.requestPermissions(
                     activity,
                     arrayOf(Manifest.permission.POST_NOTIFICATIONS),
                     0
                 )
             }
-        }else{
-            //TODO: Custom prompt for older version...
+        } else {
+            //Old version -> show custom prompt
+            val notificationManagerCompat = NotificationManagerCompat.from(context)
+            val areNotificationsEnabled = notificationManagerCompat.areNotificationsEnabled()
+
+            //Show dialog
+            if (!areNotificationsEnabled) {
+                return true
+            }
         }
+
+        return false
+    }
+
+    fun hasNotificationPermission(context: Context): Boolean {
+        val activity = context as? Activity
+            ?: return false //Return if the context is not an instance of Activity
+
+        //Check for permission
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            //Old version
+            val notificationManagerCompat = NotificationManagerCompat.from(context)
+            val areNotificationsEnabled = notificationManagerCompat.areNotificationsEnabled()
+            return areNotificationsEnabled
+        }
+    }
     }
 }
