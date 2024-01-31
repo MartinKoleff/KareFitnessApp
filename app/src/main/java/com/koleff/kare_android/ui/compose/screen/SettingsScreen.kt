f96e5d02c692c41c7f53508275d326589e85836d
MartinKoleff <martin_koleff@abv.bg>
2024-01-31 17:20:37 +0200

    Wired notification and biometrics callbacks with SettingsList composable.
    To migrate to dialog flag.

--- app/src/main/java/com/koleff/kare_android/ui/compose/screen/SettingsScreen.kt
+++ app/src/main/java/com/koleff/kare_android/ui/compose/screen/SettingsScreen.kt
@@ -4,18 +4,40 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import com.koleff.kare_android.common.PermissionManager
 import com.koleff.kare_android.ui.compose.scaffolds.MainScreenScaffold
 import com.koleff.kare_android.ui.compose.SettingsList
 
 @Composable
-fun SettingsScreen(navController: NavHostController, isNavigationInProgress: MutableState<Boolean>) {
+fun SettingsScreen(
+    navController: NavHostController,
+    isNavigationInProgress: MutableState<Boolean>
+) {
     MainScreenScaffold("Settings", navController, isNavigationInProgress) { innerPadding ->
         val modifier = Modifier
             .padding(innerPadding)
             .fillMaxSize()
 
-        SettingsList(modifier = modifier)
+        //TODO: send broadcast?
+        val context = LocalContext.current
+        val notificationCallback: () -> Unit = { PermissionManager.requestNotificationPermission(context) } //TODO: update to boolean flag...
+        val biometricsCallback: () -> Unit = {}
+        val notificationIsChecked = remember {
+            mutableStateOf(PermissionManager.hasNotificationPermission(context))
+        }
+//        val biometricsIsChecked = PermissionManager.hasBiometricsPermission(context)
+
+        SettingsList(
+            modifier = modifier,
+            notificationCallback = notificationCallback,
+            biometricsCallback = biometricsCallback,
+            notificationIsChecked = notificationIsChecked.value,
+            biometricsIsChecked = false
+        )
     }
 }
