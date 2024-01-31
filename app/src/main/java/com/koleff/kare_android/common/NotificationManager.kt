ff840b54cd88fd9da7980060e1b9f546500ec0a3
MartinKoleff <martin_koleff@abv.bg>
2024-01-31 18:19:45 +0200

    Added open notification settings system intent.

--- app/src/main/java/com/koleff/kare_android/common/NotificationManager.kt
+++ app/src/main/java/com/koleff/kare_android/common/NotificationManager.kt
@@ -2,13 +2,18 @@ package com.koleff.kare_android.common
 
 import android.Manifest
 import android.app.Activity
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.provider.Settings
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.koleff.kare_android.R
+import com.koleff.kare_android.ui.MainActivity
 
 object NotificationManager {
 
@@ -29,6 +34,7 @@ object NotificationManager {
             with(NotificationManagerCompat.from(context)) {
                 val activity = context as? Activity ?: return
 
+                //Check for permission
                 if (ActivityCompat.checkSelfPermission(
                         activity,
                         Manifest.permission.POST_NOTIFICATIONS
@@ -40,5 +46,17 @@ object NotificationManager {
                 notify(notificationId, builder.build())
             }
         }
+    }
+
+    fun openNotificationSettings(context: Context) {
+        val packageName = context.packageName
+
+        val intent = Intent().apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            }
+        }
+        startActivity(context, intent, null)
     }
 }
