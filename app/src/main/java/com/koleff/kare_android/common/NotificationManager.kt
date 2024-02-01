e53c37b28357e58c4d9e4e112e58ddd641a82c92
MartinKoleff <martin_koleff@abv.bg>
2024-02-01 13:34:50 +0200

    Added ringtone and vibration to notification builder. Removed leftover code.

--- app/src/main/java/com/koleff/kare_android/common/NotificationManager.kt
+++ app/src/main/java/com/koleff/kare_android/common/NotificationManager.kt
@@ -8,14 +8,20 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.RingtoneManager
 import android.os.Build
 import android.provider.Settings
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.Firebase
+import com.google.firebase.messaging.messaging
 import com.koleff.kare_android.R
 import com.koleff.kare_android.ui.MainActivity
+import kotlinx.coroutines.tasks.await
+
 
 object NotificationManager {
 
@@ -30,12 +36,36 @@ object NotificationManager {
         val notificationId = 1 //'notificationId' is a unique int for each notification that you must define. For testing, you can use a fixed ID.
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            //Ringtone sound
+            val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+            //Vibration
+            val pattern = longArrayOf(500, 500, 500, 500, 500)
+
+            var pendingIntent: PendingIntent? = null
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                //Pop up notification
+                val intent = Intent(context, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+                pendingIntent = PendingIntent.getActivity(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_MUTABLE //FLAG_UPDATE_CURRENT
+                )
+            }
+
             val builder = NotificationCompat.Builder(context, Constants.NOTIFICATION_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_logo)
-                .setContentTitle("Test Notification")
-                .setContentText("This is a sample notification using the channel.")
+                .setContentTitle(title)
+                .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_WORKOUT)
+                .setSound(defaultSound)
+                .setVibrate(pattern)
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(false)
                 .setOngoing(false)
 
@@ -57,7 +87,7 @@ object NotificationManager {
         startActivity(context, intent, null)
     }
 
-    fun createNotificationChannel(context: Context) = with(context){
+    fun createNotificationChannel(context: Context) = with(context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 Constants.NOTIFICATION_CHANNEL_ID,
@@ -65,7 +95,8 @@ object NotificationManager {
                 NotificationManager.IMPORTANCE_HIGH
             )
 
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
     }
