4c776a72adae84fd43c1fe620503cdfb9f4d1657
MartinKoleff <martin_koleff@abv.bg>
2024-02-01 13:50:09 +0200

    Wired FCM with topics onMessageReceived.

--- app/src/main/java/com/koleff/kare_android/service/PushNotificationService.kt
+++ app/src/main/java/com/koleff/kare_android/service/PushNotificationService.kt
@@ -3,8 +3,9 @@ package com.koleff.kare_android.service
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.koleff.kare_android.common.NotificationManager
 
-class PushNotificationService: FirebaseMessagingService() {
+class PushNotificationService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         Log.d("FCM", "New token registered: $token")
@@ -13,7 +14,25 @@ class PushNotificationService: FirebaseMessagingService() {
         //TODO: save to DB -> link to user...
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d("FCM", "From: ${remoteMessage.from}")
+
+        //Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d("FCM", "Message data payload: ${remoteMessage.data}")
+        }
+
+        //Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d("FCM", "Message Notification Body: ${it.body}")
+
+            val title = it.title ?: ""
+            val body = it.body ?: ""
+
+            val context = this@PushNotificationService
+            NotificationManager.sendNotification(context, title, body)
+        }
+
+        super.onMessageReceived(remoteMessage)
     }
 }
