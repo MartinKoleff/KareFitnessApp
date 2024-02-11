85769fc5799c09dfe00ac66e26ecc4cbde4f23c7
MartinKoleff <martin_koleff@abv.bg>
2024-02-11 20:15:24 +0200

    Fixed NavigationModule imports.

--- app/src/main/java/com/koleff/kare_android/common/di/NavigationModule.kt
+++ app/src/main/java/com/koleff/kare_android/common/di/NavigationModule.kt
@@ -1,10 +1,10 @@
 package com.koleff.kare_android.common.di
 
-import com.google.android.datatransport.runtime.dagger.Binds
-import com.google.android.datatransport.runtime.dagger.Module
 import com.koleff.kare_android.common.navigation.NavigationController
 import com.koleff.kare_android.common.navigation.NavigationControllerImpl
 import com.koleff.kare_android.common.navigation.NavigationNotifier
+import dagger.Binds
+import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
