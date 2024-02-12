cbd51fce9ea2ac3cd7c51b020d063025b4de9c7d
MartinKoleff <martin_koleff@abv.bg>
2024-02-12 23:49:28 +0200

    Refactored Destination to have route building. Wired with AppNavigation.
    ROUTE is used for AppNavigation. To create custom route you create destination and the route is initialized then you can do Destination().route and get the custom route.

--- app/src/main/java/com/koleff/kare_android/common/navigation/Destination.kt
+++ app/src/main/java/com/koleff/kare_android/common/navigation/Destination.kt
@@ -1,39 +1,100 @@
 package com.koleff.kare_android.common.navigation
 
-sealed class Destination(val route: String) {
-    object Dashboard : Destination("dashboard") //Muscle group screen
-    object Workouts : Destination("workouts") //List of all workouts created / imported
-    object MuscleGroupExercisesList :
-        Destination("muscle_group/{muscle_group_id}") { //When muscle group is selected -> show exercise list
-        fun createRoute(muscleGroupId: Int) = "muscle_group/$muscleGroupId"
+sealed interface Destination {
+    val route: String
+
+    //Dashboard screen (Muscle group screen)
+    data object Dashboard : Destination {
+
+        override val route: String
+            get() = "dashboard"
+
+        const val ROUTE = "dashboard"
     }
 
-    object ExerciseDetails :
-        Destination("exercise_details/{exercise_id}/{muscle_group_id}") { //When exercise is selected -> show details screen
-        fun createRoute(exerciseId: Int, muscleGroupId: Int) =
-            "exercise_details/$exerciseId/$muscleGroupId"
+    //List of all workouts created / imported
+    data object Workouts : Destination {
+        override val route: String
+            get() = "workouts"
+
+        const val ROUTE = "workouts"
     }
 
-    object ExerciseDetailsConfigurator :
-        Destination("exercise_details_configurator/{exercise_id}/{muscle_group_id}/{workout_id}") { //When exercise is selected in search exercises screen (opened from workout details screen) -> show configurator screen (with reps, sets, weight)
-        fun createRoute(exerciseId: Int, muscleGroupId: Int, workoutId: Int) =
-            "exercise_details_configurator/$exerciseId/$muscleGroupId/$workoutId"
+    //When muscle group is selected -> show exercise list
+    class MuscleGroupExercisesList(private val muscleGroupId: Int) : Destination {
+        override val route: String
+            get() = "muscle_group/$muscleGroupId"
+
+        companion object {
+            const val ROUTE = "muscle_group/{muscle_group_id}"
+        }
     }
 
-    object WorkoutDetails :
-        Destination("workout_details/{workout_id}") { //When workout is selected -> show details screen
-        fun createRoute(workoutId: Int) = "workout_details/$workoutId"
+    //When exercise is selected -> show details screen
+    class ExerciseDetails(private val exerciseId: Int, private val muscleGroupId: Int) :
+        Destination {
+        override val route: String
+            get() = "exercise_details/$exerciseId/$muscleGroupId"
+
+        companion object {
+            const val ROUTE = "exercise_details/{exercise_id}/{muscle_group_id}"
+        }
     }
 
-    object Settings : Destination("settings") //Settings list
-    object SearchWorkoutsScreen :
-        Destination("search_workouts/{exercise_id}") { //Select workout where exercise will be added
-        fun createRoute(exerciseId: Int) = "search_workouts/$exerciseId"
+    //When exercise is selected in search exercises screen (opened from workout details screen) ->
+    //show configurator screen (with reps, sets, weight)
+    class ExerciseDetailsConfigurator(
+        private val exerciseId: Int,
+        private val muscleGroupId: Int,
+        private val workoutId: Int
+    ) : Destination {
+        override val route: String
+            get() = "exercise_details_configurator/$exerciseId/$muscleGroupId/$workoutId"
+
+        companion object {
+            const val ROUTE =
+                "exercise_details_configurator/{exercise_id}/{muscle_group_id}/{workout_id}"
+        }
     }
 
-    object SearchExercisesScreen :
-        Destination("search_exercise/{workout_id}") { //Select exercise for current workout
-        fun createRoute(workoutId: Int) = "search_exercise/$workoutId"
+    //When workout is selected -> show details screen
+    class WorkoutDetails(private val workoutId: Int) : Destination {
+        override val route: String
+            get() = "workout_details/$workoutId"
+
+        companion object {
+            const val ROUTE =
+                "workout_details/{workout_id}"
+        }
     }
 
+    //Settings list
+    data object Settings : Destination {
+        override val route: String
+            get() = "settings"
+
+        const val ROUTE = "settings"
+    }
+
+    //Select workout where exercise will be added
+    data class SearchWorkoutsScreen(private val exerciseId: Int) : Destination {
+        override val route: String
+            get() = "search_workouts/$exerciseId"
+
+        companion object {
+            const val ROUTE =
+                "search_workouts/{exercise_id}"
+        }
+    }
+
+    //Select exercise for current workout
+    data class SearchExercisesScreen(private val workoutId: Int) : Destination {
+        override val route: String
+            get() = "search_exercise/$workoutId"
+
+        companion object {
+            const val ROUTE =
+                "search_exercise/{workout_id}"
+        }
+    }
 }
