55eb3f345bd89a9b1fc6f7dfa2637add2ab76fba
MartinKoleff <martin_koleff@abv.bg>
2024-02-14 15:51:37 +0200

    Update MockupDataGenerator.kt

--- app/src/main/java/com/koleff/kare_android/common/MockupDataGenerator.kt
+++ app/src/main/java/com/koleff/kare_android/common/MockupDataGenerator.kt
@@ -5,10 +5,12 @@ import com.koleff.kare_android.data.model.dto.ExerciseDto
 import com.koleff.kare_android.data.model.dto.ExerciseSetDto
 import com.koleff.kare_android.data.model.dto.MachineType
 import com.koleff.kare_android.data.model.dto.MuscleGroup
+import com.koleff.kare_android.data.model.dto.WorkoutDetailsDto
 import com.koleff.kare_android.data.model.dto.WorkoutDto
 import java.util.UUID
 
-object MockupDataGenerator {
+//TODO: integrate with mockk() to be random...
+object MockupDataGenerator { //TODO: move to unit test directory...
     fun generateExercise(): ExerciseDto {
         val exerciseId = 1
 
@@ -36,6 +38,25 @@ object MockupDataGenerator {
                     ""
                 )
             exercisesList.add(currentExercise)
+            exercisesList.add(currentExercise)
+        }
+
+        return exercisesList
+    }
+
+    fun generateExerciseList(muscleGroupId: Int = 0): List<ExerciseDto>{
+        val n = 5
+        val exercisesList: MutableList<ExerciseDto> = mutableListOf()
+
+        repeat(n) { index ->
+            val currentExercise =
+                ExerciseDto(
+                    index,
+                    "BARBELL BENCH PRESS $index",
+                    MuscleGroup.fromId(muscleGroupId),
+                    MachineType.BARBELL,
+                    ""
+                )
             exercisesList.add(currentExercise)
         }
 
@@ -102,5 +123,22 @@ object MockupDataGenerator {
             )
 
         return workout
+    }
+
+    fun generateWorkoutDetails(): WorkoutDetailsDto {
+        val workoutId = 1
+        val exercises = generateExerciseList() as MutableList<ExerciseDto>
+
+        val workoutDetails =
+            WorkoutDetailsDto(
+                workoutId = workoutId,
+                name = "Epic workout $workoutId",
+                description = "Description",
+                muscleGroup = MuscleGroup.fromId(workoutId),
+                exercises = exercises,
+                isSelected = false
+            )
+
+        return workoutDetails
     }
 }
