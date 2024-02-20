1deaae77ee6ac27991eff26047b7286a6a00155a
MartinKoleff <martin_koleff@abv.bg>
2024-02-20 13:04:43 +0200

    Added GetExerciseDetailsUseCase test.

--- app/src/test/java/com/koleff/kare_android/exercise/ExerciseUseCasesUnitTest.kt
+++ app/src/test/java/com/koleff/kare_android/exercise/ExerciseUseCasesUnitTest.kt
@@ -215,6 +215,38 @@ class ExerciseUseCasesUnitTest {
         assertTrue { supportedMuscleGroups.contains(fetchedExercise.muscleGroup) }
     }
 
+    @ParameterizedTest(name = "Fetches exercise details with id {0}")
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60])
+    fun `get exercise details using GetExerciseDetailsUseCase test`(exerciseId: Int) = runTest {
+        val getExerciseDetailsState =
+            exerciseUseCases.getExerciseDetailsUseCase(exerciseId).toList()
+
+        logger.i(TAG, "Get exercise details for exerciseId $exerciseId -> isLoading state raised.")
+        assertTrue { getExerciseDetailsState[0].isLoading }
+
+        logger.i(
+            TAG,
+            "Get exercise details for exerciseId $exerciseId -> isSuccessful state raised."
+        )
+        assertTrue { getExerciseDetailsState[1].isSuccessful }
+
+        val fetchedExerciseDetails = getExerciseDetailsState[1].exercise
+        logger.i(TAG, "Fetched exercise details: $fetchedExerciseDetails")
+
+        val muscleGroupRange =
+            ExerciseGenerator.getMuscleGroupRange(fetchedExerciseDetails.muscleGroup)
+
+        logger.i(
+            TAG,
+            "Assert fetched exercise details is from muscle group  ${fetchedExerciseDetails.muscleGroup} range: $muscleGroupRange"
+        )
+        assertTrue(fetchedExerciseDetails.id >= muscleGroupRange.first && fetchedExerciseDetails.id <= muscleGroupRange.second)
+
+        val supportedMuscleGroups = MuscleGroup.getSupportedMuscleGroups()
+
+        logger.i(TAG, "Assert fetched exercise details is from supported muscle groups.")
+        assertTrue { supportedMuscleGroups.contains(fetchedExerciseDetails.muscleGroup) }
+    }
 
         logger.i(TAG, "Get exercises for muscle group $muscleGroup -> isLoading state raised.")
         assertTrue { getExercisesState[0].isLoading }
