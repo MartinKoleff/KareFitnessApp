c383a6710987570f7d31ca6134e3e6bcc69b42ff
MartinKoleff <martin_koleff@abv.bg>
2024-02-29 16:44:06 +0200

    Added deployed server url. Added default user initialization.

--- app/src/main/java/com/koleff/kare_android/data/model/dto/UserDto.kt
+++ app/src/main/java/com/koleff/kare_android/data/model/dto/UserDto.kt
@@ -3,8 +3,8 @@ package com.koleff.kare_android.data.model.dto
 import java.util.UUID
 
 data class UserDto(
-    val id: UUID,
-    val username: String,
-    val password: String,
-    val email: String,
+    val id: UUID = UUID.fromString(""),
+    val username: String = "",
+    val password: String = "",
+    val email: String = "",
 )
--- app/src/main/java/com/koleff/kare_android/data/model/response/GetExercisesResponse.kt
+++ app/src/main/java/com/koleff/kare_android/data/model/response/GetExercisesResponse.kt
@@ -5,8 +5,6 @@ import com.koleff.kare_android.data.model.response.base_response.BaseResponse
 import com.squareup.moshi.Json
 
 data class GetExercisesResponse(
-//    @Json(name = "pagination")
-//    val paginationData: PaginationDataDto,
     @Json(name = "data")
     val exercises: List<ExerciseDto>
 ) : BaseResponse()
