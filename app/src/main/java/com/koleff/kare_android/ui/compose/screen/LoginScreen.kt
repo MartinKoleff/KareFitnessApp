62604cc3ef8842302ddd2ae44e3c79af4222affa
MartinKoleff <martin_koleff@abv.bg>
2024-03-02 19:52:33 +0200

    Added google login composable and horizontal line with text. Added empty register screen.

--- app/src/main/java/com/koleff/kare_android/ui/compose/screen/LoginScreen.kt
+++ app/src/main/java/com/koleff/kare_android/ui/compose/screen/LoginScreen.kt
@@ -1,20 +1,15 @@
 package com.koleff.kare_android.ui.compose.screen
 
-import android.content.Context
-import android.net.Uri
-import androidx.annotation.OptIn
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,25 +17,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.Divider
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.HorizontalAlignmentLine
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -48,261 +37,126 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.media3.common.MediaItem
-import androidx.media3.common.util.UnstableApi
-import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.AspectRatioFrameLayout
-import androidx.media3.ui.PlayerView
 import com.koleff.kare_android.R
 
 @Composable
 fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     val onLogin = {}
-    val onRegister = {}
-    val context = LocalContext.current
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopCenter
-    ) {
-
-        //Video player
-        LoginVideoPlayer()
-    }
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter
-    ) {
-
-        //Login and sign in footer
-        LoginFooter(onLogin = onLogin, onRegister = onRegister)
-    }
-}
-
-@OptIn(UnstableApi::class)
-@Composable
-fun LoginVideoPlayer() {
-    val configuration = LocalConfiguration.current
-
-    val screenHeight = configuration.screenHeightDp.dp
-    val screenWidth = configuration.screenWidthDp.dp
-
-    val context = LocalContext.current
-    val videoUri = "android.resource://${context.packageName}/${R.raw.login_video_no_watermark}"
-    val exoPlayer = remember {
-
-        //Prepare the player with the source.
-        ExoPlayer.Builder(context).build().apply {
-            val mediaItem = MediaItem.fromUri(Uri.parse(videoUri))
-            setMediaItem(mediaItem)
-            prepare()
-            playWhenReady = true //Start playing as soon as the player is ready
-            repeatMode = ExoPlayer.REPEAT_MODE_ONE //Loop the video
-        }
-    }
-
-    //Clear the exo player on dispose
-    DisposableEffect(exoPlayer) {
-        onDispose {
-            exoPlayer.release()
-        }
-    }
-
-    //Video player UI
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { ctx ->
-            PlayerView(ctx).apply {
-                player = exoPlayer
-                useController = false //Hide player controls for background playback
-                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
-            }
-        }
-    )
-}
-
-
-@Composable
-fun LoginFooter(onLogin: () -> Unit, onRegister: () -> Unit) {
-    val configuration = LocalConfiguration.current
-
-    val screenHeight = configuration.screenHeightDp.dp
-    val screenWidth = configuration.screenWidthDp.dp
-
-    val sizeModifier = Modifier
-        .fillMaxWidth()
-        .height(screenHeight / 2)
-
-    //Background
-    Box(modifier = sizeModifier){
-
-        //Texture background
-        Image(
-            painter = painterResource(id = R.drawable.metal_texture_2),
-            contentDescription = "Metal texture background",
-            modifier = Modifier
-                .alpha(0.75f)
-                .drawWithContent {
-
-                val colors = listOf(
-                    Color.Transparent,
-                    Color.Transparent,
-                    Color.Transparent,
-                    Color.Gray,
-                    Color.Gray,
-                    Color.Gray
-                )
-                drawContent()
-                drawRect(
-                    brush = Brush.verticalGradient(colors),
-                    blendMode = BlendMode.DstIn
-                )
-            },
-            contentScale = ContentScale.Crop
-        )
-    }
-
-    //Footer
     Column(
-        modifier = sizeModifier
-            .alpha(0.75f),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
+        verticalArrangement = Arrangement.Center
     ) {
 
-        //Logo
-        Image(
-            modifier = Modifier
-                .size(175.dp),
-            painter = painterResource(R.drawable.logo),
-            contentDescription = "Logo image",
-            contentScale = ContentScale.Crop
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
         )
 
-        //Login button
-        LoginButton(onLogin)
+        Spacer(modifier = Modifier.height(8.dp))
 
-        //Register/ Sign in button
-        SignInButton(onRegister)
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = onLogin) {
+            Text("Login")
+        }
     }
 }
 
 @Composable
-fun LogoRow() {
+fun HorizontalLineWithText(
+    text: String,
+    color: Color = Color.White
+) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier.padding(16.dp)
     ) {
 
-        //Welcome to
-        Text( //TODO: and cooler font...
-            modifier = Modifier.padding(
-                PaddingValues(8.dp)
-            ),
-            text = "Welcome to",
-            style = TextStyle(
-                color = MaterialTheme.colorScheme.primary,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            ),
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
-
-        //Logo
-        Image(
-            modifier = Modifier
-                .size(175.dp),
-            painter = painterResource(R.drawable.logo),
-            contentDescription = "Logo image",
-            contentScale = ContentScale.Crop
-        )
-    }
-}
-
-@Composable
-fun SignInButton(onRegister: () -> Unit) {
-    LoginRoundButton("Sign in", onRegister, hasBottomPadding = true)
-}
-
-@Composable
-fun LoginButton(onLogin: () -> Unit) {
-    LoginRoundButton("Login", onLogin, hasTopPadding = false)
-}
-
-@Composable
-fun LoginRoundButton(
-    text: String,
-    callback: () -> Unit,
-    hasBottomPadding: Boolean = false,
-    hasTopPadding: Boolean = true
-) {
-    val cornerSize = 24.dp
-
-    val paddingValues = PaddingValues(
-        start = 16.dp,
-        end = 16.dp,
-        top = if (hasTopPadding) 8.dp else 0.dp,
-        bottom = if (hasBottomPadding) 24.dp else 8.dp
-    )
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(paddingValues)
-            .height(50.dp)
-            .clip(RoundedCornerShape(cornerSize))
-            .border(
-                border = BorderStroke(2.dp, color = Color.Black),
-                shape = RoundedCornerShape(cornerSize)
-            )
-            .background(
-                color = Color.White,
-                shape = RoundedCornerShape(cornerSize)
-            )
-            .clickable(onClick = callback),
-        contentAlignment = Alignment.Center
-    ) {
-
-        //Texture background
-        Image(
-            painter = painterResource(id = R.drawable.metal_texture_3),
-            contentDescription = "Metal texture background",
-            modifier = Modifier.alpha(0.85f),
-            contentScale = ContentScale.Crop
-        )
+        //Left divider
+        Box(modifier = Modifier.weight(1f)) {
+            Divider(color = color)
+        }
 
         //Login text
-        Text( //TODO: and cooler font...
+        Text(
             modifier = Modifier.padding(
                 PaddingValues(8.dp)
             ),
             text = text,
             style = TextStyle(
-                color = Color.Black,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
+                color = color,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Thin
             ),
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
+
+        //Right divider
+        Box(modifier = Modifier.weight(1f)) {
+            Divider(color = color)
+        }
     }
 }
 
+//TODO: title with subtitle...
+//TODO: SignUpFooter...
+//TODO: background image (space theme?)
+//TODO: textFieldBox... (strong password meter?)
+@Preview
+@Composable
+fun HorizontalLineWithTextPreview() {
+    HorizontalLineWithText("Or continue with")
+}
 
-@Preview("Login Screen")
+@Composable
+fun GoogleBox() {
+    Box(
+        modifier = Modifier
+            .width(150.dp)
+            .height(75.dp)
+            .background(color = Color.Black)
+            .border(
+                border = BorderStroke(2.dp, color = Color.White),
+                shape = RoundedCornerShape(16.dp)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+
+        //Google logo
+        Image(
+            modifier = Modifier.size(50.dp),
+            painter = painterResource(R.drawable.ic_google_logo),
+            contentDescription = "Google logo image",
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview
+@Composable
+fun GoogleBoxPreview() {
+    GoogleBox()
+}
+
+@Preview
 @Composable
 fun LoginScreenPreview() {
     LoginScreen()
-}
-
-@Preview()
-@Composable
-fun LoginFooterPreview() {
-    LoginFooter(
-        onLogin = {},
-        onRegister = {}
-    )
 }
---
+++ app/src/main/java/com/koleff/kare_android/ui/compose/screen/RegisterScreen.kt
@@ -0,0 +1,15 @@
+package com.koleff.kare_android.ui.compose.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegisterScreen() {
+
+}
+
+@Preview
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen()
+}
