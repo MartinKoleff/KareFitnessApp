243aec313101dfab0e85f39b7a6fb7e6e1566a14
MartinKoleff <martin_koleff@abv.bg>
2024-03-04 21:39:21 +0200

    Added custom text field and password text field. Wired with login screen. Added icon dependency.
    To update background and image on top.

--- app/src/main/java/com/koleff/kare_android/ui/compose/screen/LoginScreen.kt
+++ app/src/main/java/com/koleff/kare_android/ui/compose/screen/LoginScreen.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,22 +17,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
-import androidx.compose.material3.Button
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,41 +56,276 @@ import com.koleff.kare_android.R
 
 @Composable
 fun LoginScreen() {
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
 
-    val onLogin = {}
+    val cornerSize = 36.dp
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    val gymImageModifier = Modifier
+        .fillMaxWidth()
+        .height(screenHeight * 1 / 4)
+    val screenContentModifier = Modifier
+        .fillMaxWidth()
+        .height(screenHeight * 3 / 4)
+
+    val onSignIn: () -> Unit = {}
+    val onGoogleSign: () -> Unit = {}
+
+    var username by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    //Needed to position background on the bottom part
+    Box(modifier = Modifier
+        .fillMaxSize()
+//        .background(Color.Black)
     ) {
 
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth()
-        )
+        //Background image
+        Box(
+            modifier = screenContentModifier //fill max size?
+                .clip(RoundedCornerShape(cornerSize))
+                .align(Alignment.BottomCenter)
+        ) {
 
-        Spacer(modifier = Modifier.height(8.dp))
+            //Texture background
+            Image(
+                painter = painterResource(id = R.drawable.ic_login_background_3),
+                contentDescription = "Background",
+                contentScale = ContentScale.Crop
+            )
+        }
 
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth()
-        )
+        //Screen
+        Column(
+            modifier = Modifier.fillMaxSize(), //screenContentModifier
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
 
-        Spacer(modifier = Modifier.height(16.dp))
+            //Gym image
+            Image(
+                modifier = gymImageModifier,
+//                .clip(RoundedCornerShape(cornerSize))
+//                .border(
+//                    border = BorderStroke(2.dp, color = Color.White),
+//                    shape = RoundedCornerShape(cornerSize)
+//                ),
+                painter = painterResource(id = R.drawable.ic_default),
+                contentDescription = "Top Image",
+                contentScale = ContentScale.Crop
+            )
 
-        Button(onClick = onLogin) {
-            Text("Login")
+            CustomTitleAndSubtitle(
+                title = "Welcome back!",
+                subtitle = "We missed you!"
+            )
+
+            //User text box
+            CustomTextField(label = "Username") {
+                username = it
+            }
+
+            //Password text box
+            PasswordTextField(label = "Password") {
+                password = it
+            }
+
+
+            SignInButton(onSignIn = onSignIn)
+            SignInFooter(onGoogleSign = onGoogleSign)
         }
     }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
+
+@Composable
+fun CustomTitleAndSubtitle(title: String, subtitle: String) {
+    val textColor = Color.White
+
+    val titlePadding =
+        PaddingValues(
+            top = 8.dp,
+            bottom = 0.dp,
+            start = 8.dp,
+            end = 8.dp
+        )
+
+    val subtitlePadding =
+        PaddingValues(
+            top = 2.dp,
+            bottom = 64.dp,
+            start = 8.dp,
+            end = 8.dp
+        )
+
+    //Title
+    Text(
+        modifier = Modifier.padding(
+            titlePadding
+        ),
+        text = title,
+        style = TextStyle(
+            color = textColor,
+            fontSize = 48.sp,
+            fontWeight = FontWeight.ExtraBold
+        ),
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+
+    //Subtitle
+    Text(
+        modifier = Modifier.padding(
+            subtitlePadding
+        ),
+        text = subtitle,
+        style = TextStyle(
+            color = textColor,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Light
+        ),
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+}
+
+@Composable
+fun CustomTextField(
+    label: String,
+    onValueChange: (String) -> Unit
+) {
+    var text by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val paddingValues = PaddingValues(
+        horizontal = 32.dp,
+        vertical = 8.dp
+    )
+    val cornerSize = 8.dp //slight round corners
+
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+            onValueChange(it)
+        },
+        label = {
+            Text(label)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues)
+            .clip(RoundedCornerShape(cornerSize))
+            .border(
+                border = BorderStroke(2.dp, color = Color.White),
+                shape = RoundedCornerShape(cornerSize)
+            ),
+        singleLine = true,
+        placeholder = {
+            Text(label)
+        },
+        leadingIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.ic_vector_profile),
+                contentDescription = "Icon",
+//                    colorFilter = ColorFilter.tint(
+//                        color = MaterialTheme.colorScheme.primary,
+//                        blendMode = BlendMode.DstIn
+//                    )
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+fun CustomTextFieldPreview() {
+    CustomTextField(
+        label = "Username",
+        onValueChange = {}
+    )
+}
+
+@Composable
+fun PasswordTextField(
+    label: String = "Password",
+    onValueChange: (String) -> Unit,
+) {
+    var password by rememberSaveable {
+        mutableStateOf("")
+    }
+    var passwordVisible by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val paddingValues = PaddingValues(
+        horizontal = 32.dp,
+        vertical = 8.dp
+    )
+    val cornerSize = 8.dp //slight round corners
+
+    TextField(
+        value = password,
+        onValueChange = {
+            password = it
+            onValueChange(it)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues)
+            .clip(RoundedCornerShape(cornerSize))
+            .border(
+                border = BorderStroke(2.dp, color = Color.White),
+                shape = RoundedCornerShape(cornerSize)
+            ),
+        label = {
+            Text(label)
+        },
+        singleLine = true,
+        placeholder = {
+            Text(label)
+        },
+        leadingIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.ic_vector_password),
+                contentDescription = "Password Icon",
+//                    colorFilter = ColorFilter.tint(
+//                        color = MaterialTheme.colorScheme.primary,
+//                        blendMode = BlendMode.DstIn
+//                    )
+            )
+        },
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val image = if (passwordVisible)
+                Icons.Filled.Visibility
+            else Icons.Filled.VisibilityOff
+
+            val contentDescription = if (passwordVisible) "Hide password" else "Show password"
+            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                Icon(imageVector = image, contentDescription)
+            }
+        })
+}
+
+@Preview
+@Composable
+fun PasswordTextFieldPreview() {
+    PasswordTextField(
+        label = "Password",
+        onValueChange = {}
+    )
 }
 
 @Composable
