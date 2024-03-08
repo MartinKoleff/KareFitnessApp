41c2b4bb770ef14acfac441779c23bb3170aa01c
MartinKoleff <martin_koleff@abv.bg>
2024-03-08 17:05:09 +0200

    Added password and credentials validation tests. Added more method sources.

--- app/src/test/java/com/koleff/kare_android/authentication/AuthenticationTest.kt
+++ app/src/test/java/com/koleff/kare_android/authentication/AuthenticationTest.kt
@@ -2,6 +2,7 @@ package com.koleff.kare_android.authentication
 
 import com.koleff.kare_android.authentication.data.CredentialsDataStoreFake
 import com.koleff.kare_android.authentication.data.UserDaoFake
+import com.koleff.kare_android.common.credentials_validator.Credentials
 import com.koleff.kare_android.common.credentials_validator.CredentialsAuthenticator
 import com.koleff.kare_android.common.credentials_validator.CredentialsAuthenticatorImpl
 import com.koleff.kare_android.common.credentials_validator.CredentialsValidator
@@ -11,6 +12,7 @@ import com.koleff.kare_android.data.datasource.AuthenticationLocalDataSource
 import com.koleff.kare_android.data.datasource.UserDataSource
 import com.koleff.kare_android.data.datasource.UserLocalDataSource
 import com.koleff.kare_android.data.model.dto.MuscleGroup
+import com.koleff.kare_android.data.model.response.base_response.KareError
 import com.koleff.kare_android.data.repository.AuthenticationRepositoryImpl
 import com.koleff.kare_android.data.repository.UserRepositoryImpl
 import com.koleff.kare_android.data.room.entity.User
@@ -19,9 +21,12 @@ import com.koleff.kare_android.domain.repository.UserRepository
 import com.koleff.kare_android.domain.usecases.AuthenticationUseCases
 import com.koleff.kare_android.domain.usecases.LoginUseCase
 import com.koleff.kare_android.domain.usecases.RegisterUseCase
+import com.koleff.kare_android.domain.wrapper.ResultWrapper
 import com.koleff.kare_android.utils.TestLogger
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertThrows
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.params.ParameterizedTest
@@ -91,6 +96,108 @@ class AuthenticationTest {
             return usernames.flatMap { username ->
                 saveToDBFlags.map { flag ->
                     Arguments.of(username, flag)
+                }
+            }.stream()
+        }
+
+        @JvmStatic
+        fun provideInvalidCredentials(): Stream<Arguments> {
+            val usernames = listOf(
+                "a",
+                "123",
+                "ivo",
+                "a3c",
+                "mk",
+                " "
+            )
+            val passwords = listOf(
+                "k0leff",
+                "123456",
+                "sliv3n",
+                "parola",
+                "a",
+                "abc"
+            )
+
+            val emails = listOf(
+                "@gmail.com",
+                "test@gmail",
+                "testgmail.com",
+                "testgmail.com",
+                "@gmail.com"
+            )
+
+            val saveToDBFlags =
+                listOf(
+                    true,
+                    false
+                )
+
+            return usernames.flatMap { username ->
+                passwords.flatMap { password ->
+                    emails.flatMap { email ->
+                        saveToDBFlags.map { flag ->
+                            Arguments.of(
+                                Credentials(
+                                    UUID.randomUUID(),
+                                    username,
+                                    password,
+                                    email
+                                ), flag
+                            )
+                        }
+                    }
+                }
+            }.stream()
+        }
+
+        @JvmStatic
+        fun provideValidCredentials(): Stream<Arguments> {
+            val usernames = listOf(
+                "koleff777",
+                "mkoleff",
+                "vankatadlg",
+                "1234567",
+                "Sliv3nT0pa"
+            )
+
+            val passwords = listOf(
+                "password",
+                "mkoleff",
+                "sliv3nT0pa",
+                "spasKoleff",
+                "valioIspaneca",
+                "1234567"
+            )
+
+            val emails = listOf(
+                "ivan@gmail.com",
+                "valio@abv.bg",
+                "martin@amazon.co.uk",
+                "koleff@kare.com",
+                "test@gmail.com.com"
+            )
+
+            val saveToDBFlags =
+                listOf(
+                    true,
+                    false
+                )
+
+            return usernames.flatMap { username ->
+                passwords.flatMap { password ->
+                    emails.flatMap { email ->
+                        saveToDBFlags.map { flag ->
+                            Arguments.of(
+                                Credentials(
+                                    UUID.randomUUID(),
+                                    username,
+                                    password,
+                                    email
+                                ), flag
+                            )
+                        }
+                    }
                 }
             }.stream()
         }
@@ -164,45 +271,75 @@ class AuthenticationTest {
         }
     }
 
-    @ParameterizedTest(name = "Validates username {0}")
+    @ParameterizedTest(name = "Validates username: {0} and save to DB: {1}")
     @MethodSource("provideValidUsernames")
-    fun `username validation test with valid usernames`(username: String, saveToDB: Boolean) = runTest {
+    fun `username validation test with valid usernames`(username: String, saveToDB: Boolean) =
+        runTest {
 
-        //Save entry to db before test...
-        if(saveToDB) {
-            userDao.saveUser(
-                User(
-                    UUID.randomUUID(),
-                    username,
-                    "password",
-                    "email@gmail.com"
+            //Save entry to db before test...
+            if (saveToDB) {
+                userDao.saveUser(
+                    User(
+                        UUID.randomUUID(),
+                        username,
+                        "password",
+                        "email@gmail.com"
+                    )
                 )
-            )
+            }
+
+            assertDoesNotThrow {
+                credentialsValidator.validateUsername(username)
+            }
         }
 
-        assertDoesNotThrow {
-            credentialsValidator.validateUsername(username)
-        }
-    }
-
-    @ParameterizedTest(name = "Validates username {0}")
+    @ParameterizedTest(name = "Validates username: {0} and save to DB: {1}")
     @MethodSource("provideInvalidUsernames")
-    fun `username validation test with invalid username`(username: String, saveToDB: Boolean) = runTest {
+    fun `username validation test with invalid username`(username: String, saveToDB: Boolean) =
+        runTest {
 
-        //Save entry to db before test...
-        if(saveToDB) {
-            userDao.saveUser(
-                User(
-                    UUID.randomUUID(),
-                    username,
-                    "password",
-                    "email@gmail.com"
+            //Save entry to db before test...
+            if (saveToDB) {
+                userDao.saveUser(
+                    User(
+                        UUID.randomUUID(),
+                        username,
+                        "password",
+                        "email@gmail.com"
+                    )
                 )
-            )
+            }
+
+            val exception: Exception? = try {
+                credentialsValidator.validateUsername(username)
+
+                //Successfully validated
+                null
+            } catch (e: IllegalArgumentException) {
+                e
+            }
+
+            assertThrows(IllegalArgumentException::class.java) {
+                throw exception ?: return@assertThrows
+            }
         }
 
+    @ParameterizedTest(name = "Validates password: {0}")
+    @CsvSource(
+        value = [
+            "k0leff",
+            "123456",
+            "bojk0",
+            "sliv3n",
+            "parola",
+            "a",
+            "ab",
+            "abc"
+        ]
+    )
+    fun `password validation test with invalid password`(password: String) = runTest {
         val exception: Exception? = try {
-            credentialsValidator.validateUsername(username)
+            credentialsValidator.validatePassword(password)
 
             //Successfully validated
             null
@@ -215,8 +352,78 @@ class AuthenticationTest {
         }
     }
 
+    @ParameterizedTest(name = "Validates password: {0}")
+    @CsvSource(
+        value = [
+            "password",
+            "mkoleff",
+            "sliv3nT0pa",
+            "spasKoleff",
+            "valioIspaneca",
+            "1234567"
+        ]
+    )
+    fun `password validation test with valid password`(password: String) = runTest {
+        assertDoesNotThrow {
+            credentialsValidator.validatePassword(password)
+        }
+    }
 
-    //TODO: password validation test...
-    //TODO: credentials validation test...
+    @ParameterizedTest(name = "Validates credentials: {0} and save to DB: {1}")
+    @MethodSource("provideValidCredentials")
+    fun `credentials validation test with valid credentials`(
+        credentials: Credentials,
+        saveToDB: Boolean
+    ) =
+        runTest {
+
+            //Save entry to db before test...
+            if (saveToDB) {
+                userDao.saveUser(
+                    credentials.toEntity()
+                )
+            }
+
+            assertDoesNotThrow {
+                when(val result = credentialsValidator.validate(credentials)){
+                    is ResultWrapper.Success -> {
+                        assertTrue(result.data.isSuccessful)
+                    }
+                    else -> {
+
+                        //Failed test
+                        assert(false)
+                    }
+                }
+            }
+        }
+
+    @ParameterizedTest(name = "Validates credentials: {0} and save to DB: {1}")
+    @MethodSource("provideInvalidCredentials")
+    fun `credentials validation test with invalid credentials`(
+        credentials: Credentials,
+        saveToDB: Boolean
+    ) = runTest {
+
+        //Save entry to db before test...
+        if (saveToDB) {
+            userDao.saveUser(
+                credentials.toEntity()
+            )
+        }
+
+        when (val result = credentialsValidator.validate(credentials)){
+            is ResultWrapper.ApiError -> {
+                assertTrue(result.error == KareError.INVALID_CREDENTIALS)
+            }
+
+            else -> {
+
+                //Failed test
+                assert(false)
+            }
+        }
+    }
+
     //TODO: login use case test
 }
