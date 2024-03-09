e22ab9d1d8bdd20dd80866d18ea10e98014040c3
MartinKoleff <martin_koleff@abv.bg>
2024-03-09 23:15:19 +0200

    Added SplashScreenViewModel and state. Wired with MainActivity.

--- app/src/main/java/com/koleff/kare_android/ui/MainActivity.kt
+++ app/src/main/java/com/koleff/kare_android/ui/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.koleff.kare_android.ui
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.koleff.kare_android.common.Constants
 import com.koleff.kare_android.common.NotificationManager
@@ -12,9 +22,9 @@ import com.koleff.kare_android.common.navigation.AppNavigation
 import com.koleff.kare_android.common.navigation.NavigationNotifier
 import com.koleff.kare_android.common.preferences.Preferences
 import com.koleff.kare_android.ui.theme.KareTheme
+import com.koleff.kare_android.ui.view_model.SplashScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.lang.RuntimeException
@@ -26,12 +36,17 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var navigationNotifier: NavigationNotifier
 
+    private val splashScreenViewModel by viewModels<SplashScreenViewModel>()
 
     @OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimationApi::class, FlowPreview::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setupSplashScreen()
+
         setContent {
             KareTheme {
+                AppNavigation(navigationNotifier, hasSignedIn = splashScreenViewModel.state.value.hasCredentials)
             }
         }
 
@@ -40,7 +55,6 @@ class MainActivity : ComponentActivity() {
             this.lifecycleScope.launch {
                 delay(5000)
 
-                NotificationManager.sendNotification(this@MainActivity, "Test Notification", "This is a sample notification using the channel.")
                 NotificationManager.sendNotification(
                     this@MainActivity,
                     "Test Notification",
@@ -55,6 +69,42 @@ class MainActivity : ComponentActivity() {
                 delay(5000)
 
                 throw RuntimeException("Testing Firebase Crashlytics")
+            }
+        }
+    }
+
+    private fun setupSplashScreen() {
+        installSplashScreen().apply {
+
+            //Duration on screen
+            setKeepOnScreenCondition {
+                !splashScreenViewModel.state.value.isSuccessful
+            }
+
+            //Animation
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.4f,
+                    0.0f
+                )
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.4f,
+                    0.0f
+                )
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
             }
         }
     }
---
+++ app/src/main/java/com/koleff/kare_android/ui/view_model/SplashScreenViewModel.kt
@@ -0,0 +1,51 @@
+package com.koleff.kare_android.ui.view_model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.koleff.kare_android.common.Constants.splashScreenDelay
+import com.koleff.kare_android.common.preferences.Preferences
+import com.koleff.kare_android.data.model.response.base_response.KareError
+import com.koleff.kare_android.ui.state.SplashScreenState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashScreenViewModel @Inject constructor(
+    private val preferences: Preferences
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(SplashScreenState())
+    val state = _state.asStateFlow()
+
+    init {
+        getCredentials()
+    }
+
+    private fun getCredentials() {
+        viewModelScope.launch {
+            delay(splashScreenDelay)
+
+            val credentials = preferences.getCredentials()
+            val tokens = preferences.getTokens()
+
+            val isSuccessful = credentials != null && tokens != null
+            if (isSuccessful) {
+                _state.value = SplashScreenState(
+                    isSuccessful = true,
+                    hasCredentials = true,
+                    credentials = credentials!!,
+                    tokens = tokens!!,
+                )
+            } else {
+                _state.value = SplashScreenState(
+                    isSuccessful = true,
+                    hasCredentials = false,
+                )
+            }
+        }
+    }
+}
