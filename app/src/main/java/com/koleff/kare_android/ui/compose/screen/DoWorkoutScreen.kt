d4597f04637eba4ed3138354c14e3980a8645cd8
MartinKoleff <martin_koleff@abv.bg>
2024-03-14 00:48:18 +0200

    Added ExerciseTimer. Added style configuration.

---
+++ app/src/main/java/com/koleff/kare_android/ui/compose/screen/DoWorkoutScreen.kt
@@ -0,0 +1,121 @@
+package com.koleff.kare_android.ui.compose.screen
+
+import android.graphics.Color
+import android.graphics.Paint
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.koleff.kare_android.common.DegreeUtils
+import com.koleff.kare_android.common.TimerUtil
+import com.koleff.kare_android.ui.state.ExerciseTimerStyle
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Timer
+import kotlin.math.cos
+import kotlin.math.sin
+
+@Composable
+fun DoWorkoutScreen() {
+//normal scaffold
+}
+
+@Preview
+@Composable
+fun DoWorkoutScreenPreview() {
+    DoWorkoutScreen()
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ExerciseTimer(
+    modifier: Modifier = Modifier,
+    timeLeft: String,
+    exerciseTimerStyle: ExerciseTimerStyle = ExerciseTimerStyle()
+) {
+    var time: String by remember {
+        mutableStateOf(timeLeft)
+    }
+
+    Canvas(modifier = modifier) {
+
+        drawContext.canvas.nativeCanvas.apply {
+            val circleCenter = Offset(x = center.x, y = height.toFloat()) //center.y
+
+            //Draw circle
+            drawCircle(
+                circleCenter.x,
+                circleCenter.y,
+                exerciseTimerStyle.timerRadius.toPx(),
+                Paint().apply {
+                    strokeWidth = 5.dp.toPx()
+                    color = Color.TRANSPARENT
+                    style = Paint.Style.FILL_AND_STROKE
+                    setShadowLayer(
+                        60f,
+                        0f,
+                        0f,
+                        Color.argb(50, 0, 0, 0) //Black
+                    )
+                }
+            )
+
+            //Timer display
+            drawText(
+                time,
+                circleCenter.x,
+                circleCenter.y - 10.dp.toPx(),
+                Paint().apply {
+                    color = Color.WHITE
+                    textSize = 20.sp.toPx()
+                    textAlign = Paint.Align.CENTER
+                }
+            )
+
+            //TODO: color the lines percentage of time left (0s left -> 100%)...
+
+            //Lines
+            for (i in 0..exerciseTimerStyle.totalLines) {
+                val lineLength = exerciseTimerStyle.lineLength.toPx()
+                val lineColor = exerciseTimerStyle.lineColor
+
+                val angleInRadian = -DegreeUtils.toRadian((i * 6).toFloat())
+                val outerRadius = exerciseTimerStyle.timerRadius.toPx()
+
+                val lineStart = Offset(
+                    x = (outerRadius - lineLength) * cos(angleInRadian) + circleCenter.x,
+                    y = (outerRadius - lineLength) * sin(angleInRadian) + circleCenter.y
+                )
+
+                val lineEnd = Offset(
+                    x = outerRadius * cos(angleInRadian) + circleCenter.x,
+                    y = outerRadius * sin(angleInRadian) + circleCenter.y
+                )
+
+                //Drawing timer lines
+                drawLine(
+                    color = lineColor,
+                    start = lineStart,
+                    end = lineEnd,
+                    strokeWidth = 5.dp.toPx()
+                )
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/koleff/kare_android/ui/state/ExerciseTimerStyle.kt
@@ -0,0 +1,12 @@
+package com.koleff.kare_android.ui.state
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class ExerciseTimerStyle(
+    val timerRadius: Dp = 150.dp,
+    val totalLines: Int = 30,
+    val lineLength: Dp = 35.dp,
+    val lineColor: Color = Color.Yellow
+)
