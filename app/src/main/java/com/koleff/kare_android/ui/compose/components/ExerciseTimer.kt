5211829d3576e562821648aec7e3cd42891640d5
MartinKoleff <martin_koleff@abv.bg>
2024-03-14 22:43:29 +0200

    Moved ExerciseTimer to new file.
    two color libraries were used and imports needed to be explicit.

---
+++ app/src/main/java/com/koleff/kare_android/ui/compose/components/ExerciseTimer.kt
@@ -0,0 +1,169 @@
+package com.koleff.kare_android.ui.compose.components
+
+import android.graphics.Color
+import android.graphics.Paint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.koleff.kare_android.common.DegreeUtils
+import com.koleff.kare_android.common.TimerUtil
+import com.koleff.kare_android.data.model.dto.ExerciseTime
+import com.koleff.kare_android.ui.state.ExerciseTimerStyle
+import kotlin.math.cos
+import kotlin.math.sin
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ExerciseTimer(
+    modifier: Modifier = Modifier,
+    timeLeft: String,
+    totalTime: ExerciseTime,
+    exerciseTimerStyle: ExerciseTimerStyle = ExerciseTimerStyle()
+) {
+    var time: String by remember {
+        mutableStateOf(timeLeft)
+    }
+
+    var hours: Float by remember {
+        mutableFloatStateOf(time.split(":")[0].toFloat())
+    }
+
+    var minutes: Float by remember {
+        mutableFloatStateOf(time.split(":")[1].toFloat())
+    }
+
+    var seconds: Float by remember {
+        mutableFloatStateOf(time.split(":")[2].toFloat())
+    }
+
+    //When timeLeft updates -> update time...
+    LaunchedEffect(timeLeft) {
+        time = timeLeft
+        hours = time.split(":")[0].toFloat()
+        minutes = time.split(":")[1].toFloat()
+        seconds = time.split(":")[2].toFloat()
+    }
+
+    //Time elapsed logic
+    val timeLeftInSeconds = remember(time) {
+        (hours * 3600 + minutes * 60 + seconds).toInt()
+    }
+
+    val percentageLeft = remember(timeLeftInSeconds) {
+        timeLeftInSeconds.toFloat() / totalTime.toSeconds()
+    }
+
+    val linesToColorDifferently = remember(percentageLeft) {
+        (exerciseTimerStyle.totalLines * percentageLeft).toInt()
+    }
+
+    Canvas(modifier = modifier) {
+        drawContext.canvas.nativeCanvas.apply {
+            val circleCenter = Offset(x = center.x, y = height.toFloat()) //center.y
+
+            //Draw circle
+            drawCircle(
+                circleCenter.x,
+                circleCenter.y,
+                exerciseTimerStyle.timerRadius.toPx(),
+                Paint().apply {
+                    strokeWidth = 5.dp.toPx()
+                    color = Color.TRANSPARENT
+                    style = Paint.Style.FILL_AND_STROKE
+                    setShadowLayer(
+                        60f,
+                        0f,
+                        0f,
+                        Color.argb(50, 0, 0, 0) //Black
+                    )
+                }
+            )
+
+            //Timer display
+            drawText(
+                time,
+                circleCenter.x,
+                circleCenter.y - 10.dp.toPx(),
+                Paint().apply {
+                    color = Color.WHITE
+                    textSize = 20.sp.toPx()
+                    textAlign = Paint.Align.CENTER
+                }
+            )
+
+            //TODO: add a layer that gradually fills the circle with the percentage taken of the time and clip it in the lines shape...
+
+            //Lines
+            for (i in 0..exerciseTimerStyle.totalLines) {
+                val lineLength = exerciseTimerStyle.lineLength.toPx()
+                val isElapsed = i >= linesToColorDifferently
+                val lineColor =
+                    if (isElapsed) exerciseTimerStyle.elapsedLineColor else exerciseTimerStyle.lineColor
+
+                val angleInRadian = -DegreeUtils.toRadian((i * 6).toFloat())
+                val outerRadius = exerciseTimerStyle.timerRadius.toPx()
+
+                val lineStart = Offset(
+                    x = (outerRadius - lineLength) * cos(angleInRadian) + circleCenter.x,
+                    y = (outerRadius - lineLength) * sin(angleInRadian) + circleCenter.y
+                )
+
+                val lineEnd = Offset(
+                    x = outerRadius * cos(angleInRadian) + circleCenter.x,
+                    y = outerRadius * sin(angleInRadian) + circleCenter.y
+                )
+
+                //Drawing timer lines
+                drawLine(
+                    color = lineColor,
+                    start = lineStart,
+                    end = lineEnd,
+                    strokeWidth = 5.dp.toPx()
+                )
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+fun ExerciseTimerPreview() {
+    val totalTime = ExerciseTime(hours = 0, minutes = 3, seconds = 30)
+    var currentTime by remember {
+        mutableStateOf(
+            String.format(
+                "%02d:%02d:%02d",
+                totalTime.hours,
+                totalTime.minutes,
+                totalTime.seconds
+            )
+        )
+    }
+
+    LaunchedEffect(Unit) {
+        TimerUtil.startTimer(totalTime.toSeconds()) {
+            currentTime = it
+        }
+    }
+
+    ExerciseTimer(
+        modifier = Modifier.fillMaxSize(),
+        timeLeft = currentTime,
+        totalTime = totalTime
+    )
+}
--- app/src/main/java/com/koleff/kare_android/ui/compose/screen/DoWorkoutScreen.kt
+++ app/src/main/java/com/koleff/kare_android/ui/compose/screen/DoWorkoutScreen.kt
@@ -1,11 +1,11 @@
 package com.koleff.kare_android.ui.compose.screen
 
-import android.graphics.Color
-import android.graphics.Paint
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -15,18 +15,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.SheetState
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -34,16 +43,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.koleff.kare_android.common.DegreeUtils
+import com.koleff.kare_android.R
+import com.koleff.kare_android.common.MockupDataGenerator
 import com.koleff.kare_android.common.TimerUtil
 import com.koleff.kare_android.common.navigation.Destination
 import com.koleff.kare_android.common.navigation.NavigationEvent
+import com.koleff.kare_android.data.model.dto.ExerciseDto
 import com.koleff.kare_android.data.model.dto.ExerciseTime
+import com.koleff.kare_android.ui.compose.components.ExerciseDataSheet
+import com.koleff.kare_android.ui.compose.components.ExerciseTimer
 import com.koleff.kare_android.ui.compose.components.navigation_components.scaffolds.DoWorkoutScaffold
 import com.koleff.kare_android.ui.state.ExerciseTimerStyle
 import com.koleff.kare_android.ui.view_model.DoWorkoutViewModel
-import kotlin.math.cos
-import kotlin.math.sin
 
 //TODO: screen between sets
 // that counts down time before next set...
@@ -52,12 +63,13 @@ import kotlin.math.sin
 // Have screen for finishing workout with stats...
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun DoWorkoutScreen() {
+fun DoWorkoutScreen(doWorkoutViewModel: DoWorkoutViewModel = hiltViewModel()) {
 
-    val time = ExerciseTime(hours = 0, minutes = 1, seconds = 30) //default time (wire with exercise time)
+    val time =
+        ExerciseTime(hours = 0, minutes = 1, seconds = 30) //default time (wire with exercise time)
 
     //Navigation Callbacks
-    val onNavigateBack = {
+    val onExitWorkoutAction = {
         doWorkoutViewModel.onNavigationEvent(
             NavigationEvent.ClearBackstackAndNavigateTo(Destination.Dashboard)
         )
@@ -65,7 +77,7 @@ fun DoWorkoutScreen() {
 
     DoWorkoutScaffold(
         screenTitle = "", //current exercise...
-        onExitWorkoutAction = onNavigateBack
+        onExitWorkoutAction = onExitWorkoutAction
     ) {
         //Video player...
 
@@ -80,7 +92,7 @@ fun DoWorkoutFooter(
     totalTime: ExerciseTime
 ) {
     val exerciseDataPadding = PaddingValues(4.dp)
-    val textColor = androidx.compose.ui.graphics.Color.White
+    val textColor = Color.White
 
     //Timer
     val hours = totalTime.hours
@@ -105,7 +117,9 @@ fun DoWorkoutFooter(
 
         //reps
         Column(
-            modifier = Modifier.weight(1f).fillMaxHeight(),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -145,14 +159,18 @@ fun DoWorkoutFooter(
         }
 
         ExerciseTimer(
-            modifier = Modifier.weight(2.5f).size(exerciseTimerStyle.timerRadius),
+            modifier = Modifier
+                .weight(2.5f)
+                .size(exerciseTimerStyle.timerRadius),
             timeLeft = currentTime,
             totalTime = totalTime
         )
 
         //weight
         Column(
-            modifier = Modifier.weight(1f).fillMaxHeight(),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -188,152 +206,11 @@ fun DoWorkoutFooter(
     //TODO: add slider below for exercise data sheet...
 }
 
+
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun DoWorkoutFooterPreview() {
     val time = ExerciseTime(hours = 0, minutes = 5, seconds = 30)
     DoWorkoutFooter(totalTime = time)
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun ExerciseTimer(
-    modifier: Modifier = Modifier,
-    timeLeft: String,
-    totalTime: ExerciseTime,
-    exerciseTimerStyle: ExerciseTimerStyle = ExerciseTimerStyle()
-) {
-    var time: String by remember {
-        mutableStateOf(timeLeft)
-    }
-
-    var hours: Float by remember {
-        mutableFloatStateOf(time.split(":")[0].toFloat())
-    }
-
-    var minutes: Float by remember {
-        mutableFloatStateOf(time.split(":")[1].toFloat())
-    }
-
-    var seconds: Float by remember {
-        mutableFloatStateOf(time.split(":")[2].toFloat())
-    }
-
-    //When timeLeft updates -> update time...
-    LaunchedEffect(timeLeft) {
-        time = timeLeft
-        hours = time.split(":")[0].toFloat()
-        minutes = time.split(":")[1].toFloat()
-        seconds = time.split(":")[2].toFloat()
-    }
-
-    //Time elapsed logic
-    val timeLeftInSeconds = remember(time) {
-        (hours * 3600 + minutes * 60 + seconds).toInt()
-    }
-
-    val percentageLeft = remember(timeLeftInSeconds) {
-        timeLeftInSeconds.toFloat() / totalTime.toSeconds()
-    }
-
-    val linesToColorDifferently = remember(percentageLeft) {
-        (exerciseTimerStyle.totalLines * percentageLeft).toInt()
-    }
-
-    Canvas(modifier = modifier) {
-        drawContext.canvas.nativeCanvas.apply {
-            val circleCenter = Offset(x = center.x, y = height.toFloat()) //center.y
-
-            //Draw circle
-            drawCircle(
-                circleCenter.x,
-                circleCenter.y,
-                exerciseTimerStyle.timerRadius.toPx(),
-                Paint().apply {
-                    strokeWidth = 5.dp.toPx()
-                    color = Color.TRANSPARENT
-                    style = Paint.Style.FILL_AND_STROKE
-                    setShadowLayer(
-                        60f,
-                        0f,
-                        0f,
-                        Color.argb(50, 0, 0, 0) //Black
-                    )
-                }
-            )
-
-            //Timer display
-            drawText(
-                time,
-                circleCenter.x,
-                circleCenter.y - 10.dp.toPx(),
-                Paint().apply {
-                    color = Color.WHITE
-                    textSize = 20.sp.toPx()
-                    textAlign = Paint.Align.CENTER
-                }
-            )
-
-            //TODO: add a layer that gradually fills the circle with the percentage taken of the time and clip it in the lines shape...
-
-            //Lines
-            for (i in 0..exerciseTimerStyle.totalLines) {
-                val lineLength = exerciseTimerStyle.lineLength.toPx()
-                val isElapsed = i >= linesToColorDifferently
-                val lineColor =
-                    if (isElapsed) exerciseTimerStyle.elapsedLineColor else exerciseTimerStyle.lineColor
-
-                val angleInRadian = -DegreeUtils.toRadian((i * 6).toFloat())
-                val outerRadius = exerciseTimerStyle.timerRadius.toPx()
-
-                val lineStart = Offset(
-                    x = (outerRadius - lineLength) * cos(angleInRadian) + circleCenter.x,
-                    y = (outerRadius - lineLength) * sin(angleInRadian) + circleCenter.y
-                )
-
-                val lineEnd = Offset(
-                    x = outerRadius * cos(angleInRadian) + circleCenter.x,
-                    y = outerRadius * sin(angleInRadian) + circleCenter.y
-                )
-
-                //Drawing timer lines
-                drawLine(
-                    color = lineColor,
-                    start = lineStart,
-                    end = lineEnd,
-                    strokeWidth = 5.dp.toPx()
-                )
-            }
-        }
-    }
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
-@Preview
-@Composable
-fun ExerciseTimerPreview() {
-    val totalTime = ExerciseTime(hours = 0, minutes = 3, seconds = 30)
-    var currentTime by remember {
-        mutableStateOf(
-            String.format(
-                "%02d:%02d:%02d",
-                totalTime.hours,
-                totalTime.minutes,
-                totalTime.seconds
-            )
-        )
-    }
-
-    LaunchedEffect(Unit) {
-        TimerUtil.startTimer(totalTime.toSeconds()) {
-            currentTime = it
-        }
-    }
-
-    ExerciseTimer(
-        modifier = Modifier.fillMaxSize(),
-        timeLeft = currentTime,
-        totalTime = totalTime
-    )
 }
