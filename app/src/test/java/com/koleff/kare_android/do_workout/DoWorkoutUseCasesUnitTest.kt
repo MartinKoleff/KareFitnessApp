f23b71caf9d565f517918348a7816cd521fdec7d
MartinKoleff <martin_koleff@abv.bg>
2024-04-25 16:05:13 +0300

    Added start and reset timer use cases tests.

--- app/src/test/java/com/koleff/kare_android/do_workout/DoWorkoutUseCasesUnitTest.kt
+++ app/src/test/java/com/koleff/kare_android/do_workout/DoWorkoutUseCasesUnitTest.kt
@@ -1,11 +1,14 @@
 package com.koleff.kare_android.do_workout
 
 import com.koleff.kare_android.common.MockupDataGenerator
-import com.koleff.kare_android.data.model.dto.ExerciseDto
-import com.koleff.kare_android.data.model.dto.MuscleGroup
+import com.koleff.kare_android.data.datasource.DoWorkoutLocalDataSource
+import com.koleff.kare_android.data.model.dto.ExerciseTime
+import com.koleff.kare_android.data.repository.DoWorkoutRepositoryImpl
 import com.koleff.kare_android.data.repository.ExerciseRepositoryImpl
 import com.koleff.kare_android.data.repository.WorkoutRepositoryImpl
 import com.koleff.kare_android.data.room.manager.ExerciseDBManager
+import com.koleff.kare_android.do_workout.data.TimerUtilFake
+import com.koleff.kare_android.domain.repository.DoWorkoutRepository
 import com.koleff.kare_android.domain.repository.ExerciseRepository
 import com.koleff.kare_android.domain.repository.WorkoutRepository
 import com.koleff.kare_android.domain.usecases.AddExerciseUseCase
@@ -17,6 +20,8 @@ import com.koleff.kare_android.domain.usecases.DeleteExerciseSetUseCase
 import com.koleff.kare_android.domain.usecases.DeleteExerciseUseCase
 import com.koleff.kare_android.domain.usecases.DeleteWorkoutUseCase
 import com.koleff.kare_android.domain.usecases.DeselectWorkoutUseCase
+import com.koleff.kare_android.domain.usecases.DoWorkoutInitialSetupUseCase
+import com.koleff.kare_android.domain.usecases.DoWorkoutUseCases
 import com.koleff.kare_android.domain.usecases.ExerciseUseCases
 import com.koleff.kare_android.domain.usecases.GetAllWorkoutDetailsUseCase
 import com.koleff.kare_android.domain.usecases.GetAllWorkoutsUseCase
@@ -31,31 +36,34 @@ import com.koleff.kare_android.domain.usecases.GetWorkoutsDetailsUseCase
 import com.koleff.kare_android.domain.usecases.OnFilterExercisesUseCase
 import com.koleff.kare_android.domain.usecases.OnSearchExerciseUseCase
 import com.koleff.kare_android.domain.usecases.OnSearchWorkoutUseCase
+import com.koleff.kare_android.domain.usecases.ResetTimerUseCase
+import com.koleff.kare_android.domain.usecases.SelectNextExerciseUseCase
 import com.koleff.kare_android.domain.usecases.SelectWorkoutUseCase
+import com.koleff.kare_android.domain.usecases.SkipNextExerciseUseCase
+import com.koleff.kare_android.domain.usecases.StartTimerUseCase
 import com.koleff.kare_android.domain.usecases.SubmitExerciseUseCase
+import com.koleff.kare_android.domain.usecases.UpdateExerciseSetsAfterTimerUseCase
 import com.koleff.kare_android.domain.usecases.UpdateWorkoutDetailsUseCase
 import com.koleff.kare_android.domain.usecases.UpdateWorkoutUseCase
 import com.koleff.kare_android.domain.usecases.WorkoutUseCases
+import com.koleff.kare_android.domain.wrapper.ResultWrapper
 import com.koleff.kare_android.exercise.ExerciseFakeDataSource
-import com.koleff.kare_android.exercise.ExerciseUseCasesUnitTest
 import com.koleff.kare_android.exercise.data.ExerciseDaoFake
 import com.koleff.kare_android.exercise.data.ExerciseDetailsDaoFake
 import com.koleff.kare_android.exercise.data.ExerciseSetDaoFake
 import com.koleff.kare_android.utils.TestLogger
 import com.koleff.kare_android.workout.WorkoutFakeDataSource
-import com.koleff.kare_android.workout.WorkoutUseCasesUnitTest
 import com.koleff.kare_android.workout.data.WorkoutDaoFake
 import com.koleff.kare_android.workout.data.WorkoutDetailsDaoFake
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.provider.Arguments
-import java.util.stream.Stream
+
+typealias DoWorkoutFakeDataSource = DoWorkoutLocalDataSource
 
 class DoWorkoutUseCasesUnitTest {
     private lateinit var exerciseDBManager: ExerciseDBManager
@@ -68,12 +76,17 @@ class DoWorkoutUseCasesUnitTest {
 
     private lateinit var exerciseFakeDataSource: ExerciseFakeDataSource
     private lateinit var workoutFakeDataSource: WorkoutFakeDataSource
+    private lateinit var doWorkoutFakeDataSource: DoWorkoutFakeDataSource
+
     private lateinit var exerciseRepository: ExerciseRepository
     private lateinit var workoutRepository: WorkoutRepository
+    private lateinit var doWorkoutRepository: DoWorkoutRepository
 
     private lateinit var exerciseUseCases: ExerciseUseCases
     private lateinit var workoutUseCases: WorkoutUseCases
+    private lateinit var doWorkoutUseCases: DoWorkoutUseCases
 
+    private lateinit var timer: TimerUtilFake
 
     private val useMockupDataSource = false
     private val isErrorTesting = false
@@ -152,6 +165,22 @@ class DoWorkoutUseCasesUnitTest {
             createCustomWorkoutDetailsUseCase = CreateCustomWorkoutDetailsUseCase(workoutRepository)
         )
 
+        //Do workout
+        doWorkoutFakeDataSource = DoWorkoutFakeDataSource()
+        doWorkoutRepository = DoWorkoutRepositoryImpl(doWorkoutFakeDataSource)
+        doWorkoutUseCases = DoWorkoutUseCases(
+            doWorkoutInitialSetupUseCase = DoWorkoutInitialSetupUseCase(doWorkoutRepository),
+            selectNextExerciseUseCase = SelectNextExerciseUseCase(doWorkoutRepository),
+            skipNextExerciseUseCase = SkipNextExerciseUseCase(doWorkoutRepository),
+            updateExerciseSetsAfterTimerUseCase = UpdateExerciseSetsAfterTimerUseCase(
+                doWorkoutRepository
+            ),
+            addNewExerciseSetUseCase = AddNewExerciseSetUseCase(exerciseRepository),
+            deleteExerciseSetUseCase = DeleteExerciseSetUseCase(exerciseRepository),
+            startTimerUseCase = StartTimerUseCase(),
+            resetTimerUseCase = ResetTimerUseCase()
+        )
+
         //Initialize DB
         exerciseDBManager = ExerciseDBManager(
             exerciseSetDao = exerciseSetDao,
@@ -163,6 +192,8 @@ class DoWorkoutUseCasesUnitTest {
         exerciseDBManager.initializeExerciseTable {
             logger.i(TAG, "DB initialized successfully!")
         }
+
+        timer = TimerUtilFake()
     }
 
 
@@ -220,12 +251,21 @@ class DoWorkoutUseCasesUnitTest {
 
             //Fetch the exercise from DB to see the changes
             val getExerciseState =
-                exerciseUseCases.getExerciseUseCase(selectedExercise.exerciseId, workoutDetails.workoutId).toList()
+                exerciseUseCases.getExerciseUseCase(
+                    selectedExercise.exerciseId,
+                    workoutDetails.workoutId
+                ).toList()
 
-            logger.i(TAG, "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isLoading state raised.")
+            logger.i(
+                TAG,
+                "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isLoading state raised."
+            )
             assertTrue { getExerciseState[0].isLoading }
 
-            logger.i(TAG, "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isSuccessful state raised.")
+            logger.i(
+                TAG,
+                "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isSuccessful state raised."
+            )
             assertTrue { getExerciseState[1].isSuccessful }
 
             val fetchedExercise = getExerciseState[1].exercise
@@ -242,7 +282,8 @@ class DoWorkoutUseCasesUnitTest {
                 exerciseUseCases.deleteExerciseSetUseCase(
                     selectedExercise.exerciseId,
                     workoutDetails.workoutId,
-                    selectedExercise.sets[0].setId ?: throw NoSuchElementException("Invalid exercise set")
+                    selectedExercise.sets[0].setId
+                        ?: throw NoSuchElementException("Invalid exercise set")
                 ).toList()
 
             logger.i(TAG, "Delete exercise set -> isLoading state raised.")
@@ -259,12 +300,21 @@ class DoWorkoutUseCasesUnitTest {
 
             //Fetch the exercise from DB to see the changes
             val getExerciseState2 =
-                exerciseUseCases.getExerciseUseCase(selectedExercise.exerciseId, workoutDetails.workoutId).toList()
+                exerciseUseCases.getExerciseUseCase(
+                    selectedExercise.exerciseId,
+                    workoutDetails.workoutId
+                ).toList()
 
-            logger.i(TAG, "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isLoading state raised.")
+            logger.i(
+                TAG,
+                "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isLoading state raised."
+            )
             assertTrue { getExerciseState2[0].isLoading }
 
-            logger.i(TAG, "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isSuccessful state raised.")
+            logger.i(
+                TAG,
+                "Get exercise for exerciseId ${selectedExercise.exerciseId} -> isSuccessful state raised."
+            )
             assertTrue { getExerciseState2[1].isSuccessful }
 
             val fetchedExercise2 = getExerciseState2[1].exercise
@@ -275,5 +325,58 @@ class DoWorkoutUseCasesUnitTest {
 
             logger.i(TAG, "Assert first exercise set was not in payload.")
             assertTrue { !fetchedExercise2.sets.contains(selectedExercise.sets[0]) }
+        }
+
+    @RepeatedTest(3)
+    fun `start timer using StartTimerUseCase test and reset timer using ResetTimerUseCase test`() =
+        runTest {
+
+            /**
+             * Start timer test
+             */
+            val defaultTime = ExerciseTime(0, 1, 0)
+
+            val startTimerState = doWorkoutUseCases.startTimerUseCase(timer, defaultTime)
+                .take(5).toList()
+
+            logger.i(TAG, "Assert there are exactly 5 updates.")
+            assertTrue { startTimerState.size == 5 }
+
+            logger.i(TAG, "Assert all states are successful.")
+            assertTrue { startTimerState.all { it is ResultWrapper.Success } }
+
+            val times = listOf(
+                (startTimerState[0] as ResultWrapper.Success).data.time,
+                (startTimerState[1] as ResultWrapper.Success).data.time,
+                (startTimerState[2] as ResultWrapper.Success).data.time,
+                (startTimerState[3] as ResultWrapper.Success).data.time,
+                (startTimerState[4] as ResultWrapper.Success).data.time,
+            )
+            val expectedTimes = listOf(
+                ExerciseTime(0, 1, 0),
+                ExerciseTime(0, 0, 59),
+                ExerciseTime(0, 0, 58),
+                ExerciseTime(0, 0, 57),
+                ExerciseTime(0, 0, 56)
+            )
+
+            logger.i(
+                TAG,
+                "Assert all times are as expected values.\nExpected times: $expectedTimes.\nTimes received from Timer: $times"
+            )
+            assertTrue { times == expectedTimes }
+
+            /**
+             * Reset timer test
+             */
+            val resetTimerState = doWorkoutUseCases.resetTimerUseCase(timer, defaultTime)
+                .toList()
+
+            logger.i(TAG, "Assert reset state is successful.")
+            assertTrue { resetTimerState[0] is ResultWrapper.Success }
+
+            val resetTime = (resetTimerState[0] as ResultWrapper.Success).data.time
+            logger.i(TAG, "Assert reset time is as starting time. Reset time: $resetTime.")
+            assertTrue { resetTime == defaultTime }
         }
 }
