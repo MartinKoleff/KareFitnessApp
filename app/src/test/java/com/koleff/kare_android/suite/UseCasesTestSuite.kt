02421710b6872fc152149425e7b84bfc7b9462f9
MartinKoleff <martin_koleff@abv.bg>
2024-04-29 00:40:22 +0300

    Update UseCasesTestSuite.kt

--- app/src/test/java/com/koleff/kare_android/suite/UseCasesTestSuite.kt
+++ app/src/test/java/com/koleff/kare_android/suite/UseCasesTestSuite.kt
@@ -1,18 +1,32 @@
 package com.koleff.kare_android.suite
 
+import com.koleff.kare_android.authentication.AuthenticationTest
+import com.koleff.kare_android.do_workout.DoWorkoutUseCasesUnitTest
+import com.koleff.kare_android.do_workout_performance_metrics.DoWorkoutPerformanceMetricsUseCasesUnitTest
+import com.koleff.kare_android.exercise.ExerciseUseCasesUnitTest
 import com.koleff.kare_android.workout.WorkoutUseCasesUnitTest
 import org.junit.platform.suite.api.IncludePackages
 import org.junit.platform.suite.api.SelectClasses
 import org.junit.platform.suite.api.SelectPackages
 import org.junit.platform.suite.api.Suite
+import org.junit.platform.suite.api.SuiteDisplayName
+import org.junit.runner.RunWith
 
 @Suite
-@SelectPackages(
-    "com.koleff.kare_android.workout",
-    "com.koleff.kare_android.exercise",
-    "do_workout",
-    "do_workout_performance_metrics",
-    "authentication"
+//@SelectPackages(
+//    "com.koleff.kare_android.workout",
+//    "com.koleff.kare_android.exercise",
+//    "do_workout",
+//    "do_workout_performance_metrics",
+//    "authentication"
+//)
+@SelectClasses(
+    WorkoutUseCasesUnitTest::class,
+    ExerciseUseCasesUnitTest::class,
+    DoWorkoutUseCasesUnitTest::class,
+    DoWorkoutPerformanceMetricsUseCasesUnitTest::class,
+    AuthenticationTest::class
 )
+@SuiteDisplayName("Use cases test suite")
 class UseCasesTestSuite {
 }
