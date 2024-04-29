08db84adb31faae1104917417d6c75e07f02e978
MartinKoleff <martin_koleff@abv.bg>
2024-04-29 23:18:48 +0300

    Added WorkoutConfigurationDaoFake impl.
    Updating workoutDetailsDaoFake DB when insert/update happens. To wire delete with workoutDetailsDaoFake...

--- app/src/test/java/com/koleff/kare_android/workout/data/WorkoutConfigurationDaoFake.kt
+++ app/src/test/java/com/koleff/kare_android/workout/data/WorkoutConfigurationDaoFake.kt
@@ -1,27 +1,60 @@
 package com.koleff.kare_android.workout.data
 
+import com.koleff.kare_android.data.model.dto.WorkoutConfigurationDto
 import com.koleff.kare_android.data.room.dao.WorkoutConfigurationDao
 import com.koleff.kare_android.data.room.dao.WorkoutConfigurationId
 import com.koleff.kare_android.data.room.entity.WorkoutConfiguration
+import com.koleff.kare_android.data.room.entity.WorkoutDetailsWithExercises
 
-class WorkoutConfigurationDaoFake: WorkoutConfigurationDao {
-    override suspend fun insertWorkoutConfiguration(config: WorkoutConfiguration): WorkoutConfigurationId {
-        TODO("Not yet implemented")
+class WorkoutConfigurationDaoFake(
+    private val workoutDetailsDao: WorkoutDetailsDaoFake
+): WorkoutConfigurationDao {
+
+    private val workoutConfigurationDB = mutableListOf<WorkoutConfiguration>()
+
+    override suspend fun insertWorkoutConfiguration(configuration: WorkoutConfiguration): WorkoutConfigurationId {
+        workoutConfigurationDB.add(configuration)
+
+        //Update workoutDetailsDao DB...
+        workoutDetailsDao.updateWorkoutConfiguration(configuration)
+
+        return workoutConfigurationDB.size.toLong()
     }
 
     override suspend fun getWorkoutConfiguration(workoutId: Int): WorkoutConfiguration? {
-        TODO("Not yet implemented")
+        return workoutConfigurationDB.firstOrNull { it.workoutId == workoutId }
     }
 
-    override suspend fun updateWorkoutConfiguration(config: WorkoutConfiguration) {
-        TODO("Not yet implemented")
+    override suspend fun updateWorkoutConfiguration(configuration: WorkoutConfiguration) {
+        val index = workoutConfigurationDB.indexOfFirst { it.workoutId == configuration.workoutId }
+
+        //WorkoutConfiguration found
+        if (index != -1) {
+
+            //Replace workout configuration
+            workoutConfigurationDB[index] = configuration
+
+            //Update workoutDetailsDao DB...
+            workoutDetailsDao.updateWorkoutConfiguration(configuration)
+        } else {
+
+            //Delete invalid workout configuration?
+        }
     }
 
-    override suspend fun deleteWorkoutConfiguration(config: WorkoutConfiguration) {
-        TODO("Not yet implemented")
+    override suspend fun deleteWorkoutConfiguration(configuration: WorkoutConfiguration) {
+        workoutConfigurationDB.remove(configuration)
     }
 
     override suspend fun deleteWorkoutConfiguration(workoutConfigurationId: Int) {
-        TODO("Not yet implemented")
+        workoutConfigurationDB.removeAll {it.workoutId == workoutConfigurationId }
+    }
+
+    fun clearDB(){
+        workoutConfigurationDB.clear()
+    }
+
+    fun getAllWorkoutConfigurations(): List<WorkoutConfiguration> {
+        return workoutConfigurationDB
     }
 }
