a83bac2f5401c1331d40250438b61d3e3f6ce469
MartinKoleff <martin_koleff@abv.bg>
2024-05-05 23:18:05 +0300

    Updated typography. Added new font.

--- app/src/main/java/com/koleff/kare_android/ui/theme/Type.kt
+++ app/src/main/java/com/koleff/kare_android/ui/theme/Type.kt
@@ -2,33 +2,150 @@ package com.koleff.kare_android.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.koleff.kare_android.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val PurpleSmileFontFamily = FontFamily(
+    listOf(
+        Font(R.font.purple_smile)
     )
-    /* Other default text styles to override
+)
+
+val Typography = Typography(
+    // Display Large - Purple smile 57/64 . -0.25px
+    displayLarge = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.25).sp,
+    ),
+
+    // Display Medium - Purple smile 45/52 . 0px
+    displayMedium = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Display Small - Purple smile 36/44 . 0px
+    displaySmall = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Headline Large - Purple smile 32/40 . 0px
+    headlineLarge = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Headline Medium - Purple smile 28/36 . 0px
+    headlineMedium = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Headline Small - Purple smile 24/32 . 0px
+    headlineSmall = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Title Large - Purple smile 22/28 . 0px
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
     ),
+
+    // Title Medium - Purple smile 16/24 . 0.15px
+    titleMedium = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+    ),
+
+    // Title Small - Purple smile 14/20 . 0.1px
+    titleSmall = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+
+    // Label Large - Purple smile 14/20 . 0.1px
+    labelLarge = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+
+    // Label Medium - Purple smile 12/16 . 0.5px
+    labelMedium = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+    ),
+
+    // Label Small - Purple smile 11/16 . 0.5px
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W500,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.5.sp,
+    ),
+
+    // Body Large - Purple smile 16/24 . 0.5px
+    bodyLarge = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+    ),
+
+    // Body Medium - Purple smile 14/20 . 0.25px
+    bodyMedium = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+    ),
+    // Body Small - Purple smile 12/16 . 0.4px
+    bodySmall = TextStyle(
+        fontFamily = PurpleSmileFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp,
+    ),
 )
