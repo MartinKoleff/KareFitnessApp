9bebb00a06f945eb540ad1a6e356c5fdebfa6658
MartinKoleff <martin_koleff@abv.bg>
2024-05-22 00:11:36 +0300

    Updated regenerateToken use case state. Added TokenState.

--- app/src/main/java/com/koleff/kare_android/domain/usecases/RegenerateTokenUseCase.kt
+++ app/src/main/java/com/koleff/kare_android/domain/usecases/RegenerateTokenUseCase.kt
@@ -5,19 +5,19 @@ import com.koleff.kare_android.data.model.dto.Tokens
 import com.koleff.kare_android.data.model.response.base_response.KareError
 import com.koleff.kare_android.domain.repository.AuthenticationRepository
 import com.koleff.kare_android.domain.wrapper.ResultWrapper
-import com.koleff.kare_android.ui.state.BaseState
+import com.koleff.kare_android.ui.state.TokenState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class RegenerateTokenUseCase(
     private val authenticationRepository: AuthenticationRepository
 ) {
-    suspend operator fun invoke(tokens: Tokens): Flow<BaseState> = flow {
+    suspend operator fun invoke(tokens: Tokens): Flow<TokenState> = flow {
         authenticationRepository.regenerateToken(tokens).collect { apiResult ->
             when (apiResult) {
                 is ResultWrapper.ApiError -> {
                     emit(
-                        BaseState(
+                        TokenState(
                             isError = true,
                             error = apiResult.error ?: KareError.GENERIC
                         )
@@ -26,15 +26,16 @@ class RegenerateTokenUseCase(
 
                 is ResultWrapper.Loading -> {
                     emit(
-                        BaseState(isLoading = true)
+                        TokenState(isLoading = true)
                     )
                 }
 
                 is ResultWrapper.Success -> {
                     Log.d("RegenerateTokenUseCase", "Token regenerated successfully! New token: ${apiResult.data.tokens}")
                     emit(
-                        BaseState(
+                        TokenState(
                             isSuccessful = true,
+                            tokens = apiResult.data.tokens
                         )
                     )
                 }
