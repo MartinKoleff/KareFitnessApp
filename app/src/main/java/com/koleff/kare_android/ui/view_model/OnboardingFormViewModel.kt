f1238f7e49ba5354049fcbc0cfb6d9d66c329eef
Martin Kolev <martin.kolev@iteco.bg>
2025-01-27 11:58:29 +0200

    Added view model scope for complete onboarding functionality.

--- app/src/main/java/com/koleff/kare_android/ui/view_model/OnboardingFormViewModel.kt
+++ app/src/main/java/com/koleff/kare_android/ui/view_model/OnboardingFormViewModel.kt
@@ -1,6 +1,7 @@
 package com.koleff.kare_android.ui.view_model
 
 import android.util.Log
+import androidx.lifecycle.viewModelScope
 import com.koleff.kare_android.common.di.MainDispatcher
 import com.koleff.kare_android.common.preferences.Preferences
 import com.koleff.kare_android.common.navigation.NavigationController
@@ -14,6 +15,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -34,27 +36,29 @@ class OnboardingFormViewModel @Inject constructor(
     }
 
     override fun clearError() {
-        if(saveOnboardingDataState.value.isError){
+        if (saveOnboardingDataState.value.isError) {
             _saveOnboardingDataState.value = BaseState()
         }
     }
 
-    suspend fun completeOnboarding(
+    fun completeOnboarding(
         gender: Gender,
         height: Int,
         age: Int,
         weight: Int
     ) {
-        Log.d("OnboardingFormViewModel", "Onboarding data: $gender, $height, $age, $weight")
-        onboardingUseCases.saveOnboardingUseCase(
-            OnboardingDataDto(
-                gender = gender,
-                height = height,
-                age = age,
-                weight = weight
-            )
-        ).collect { saveOnboardingDataState ->
-            _saveOnboardingDataState.value = saveOnboardingDataState
+        viewModelScope.launch(dispatcher) {
+            Log.d("OnboardingFormViewModel", "Onboarding data: $gender, $height, $age, $weight")
+            onboardingUseCases.saveOnboardingUseCase(
+                OnboardingDataDto(
+                    gender = gender,
+                    height = height,
+                    age = age,
+                    weight = weight
+                )
+            ).collect { saveOnboardingDataState ->
+                _saveOnboardingDataState.value = saveOnboardingDataState
+            }
         }
     }
 }
