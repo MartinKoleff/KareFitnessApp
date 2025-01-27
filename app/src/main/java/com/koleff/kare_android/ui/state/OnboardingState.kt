0c988490c8332033c93e29b8b729b14355372f4e
Martin Kolev <martin.kolev@iteco.bg>
2025-01-27 11:49:48 +0200

    Added onboarding use cases. Added DI.

---
+++ app/src/main/java/com/koleff/kare_android/ui/state/OnboardingState.kt
@@ -0,0 +1,13 @@
+package com.koleff.kare_android.ui.state
+
+import com.koleff.kare_android.data.model.dto.OnboardingDataDto
+import com.koleff.kare_android.data.model.dto.WorkoutDto
+import com.koleff.kare_android.data.model.response.base_response.KareError
+
+data class OnboardingState (
+    val onboardingData: OnboardingDataDto = OnboardingDataDto(),
+    override val isSuccessful: Boolean = false,
+    override val isLoading: Boolean = false,
+    override val isError: Boolean = false,
+    override val error: KareError = KareError.GENERIC
+): BaseState(isSuccessful, isLoading, isError, error)
