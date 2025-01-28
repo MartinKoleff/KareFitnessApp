ea29cd77e1b689cea3f4ae68af330ac7964a4c1a
Martin Kolev <martin.kolev@iteco.bg>
2025-01-28 15:49:30 +0200

    Wired focus manager for all dialogs.

--- app/src/main/java/com/koleff/kare_android/ui/compose/dialogs/WorkoutConfigurationDialog.kt
+++ app/src/main/java/com/koleff/kare_android/ui/compose/dialogs/WorkoutConfigurationDialog.kt
@@ -2,12 +2,34 @@
 
 package com.koleff.kare_android.ui.compose.dialogs
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -21,6 +43,11 @@ fun WorkoutConfigurationDialog(
     onSave: (WorkoutConfigurationDto) -> Unit,
     onDismiss: () -> Unit,
 ) {
+
+    //Keyboard
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+
     var cooldownHours by remember { mutableStateOf(workoutConfiguration.cooldownTime.hours) }
     var cooldownMinutes by remember { mutableStateOf(workoutConfiguration.cooldownTime.minutes) }
     var cooldownSeconds by remember { mutableStateOf(workoutConfiguration.cooldownTime.seconds) }
@@ -126,6 +153,17 @@ fun WorkoutConfigurationDialog(
                         colors = OutlinedTextFieldDefaults.colors(
                             focusedBorderColor = outlineColor,
                             unfocusedBorderColor = outlineColor
+                        ),
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Send
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onSend = {
+                                keyboardController?.hide()
+                                focusManager.clearFocus()
+
+                                onSave(workoutConfiguration)
+                            }
                         )
                     )
                 }
