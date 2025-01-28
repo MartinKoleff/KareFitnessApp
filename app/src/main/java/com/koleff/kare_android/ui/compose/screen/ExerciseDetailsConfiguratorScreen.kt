2f80ffa2d4b0f68a6b5899958e4dd2c48bd39460
Martin Kolev <martin.kolev@iteco.bg>
2025-01-28 11:03:27 +0200

    Fixed configuration change bugs in ExerciseDetailsConfiguratorScreen.

--- app/src/main/java/com/koleff/kare_android/ui/compose/screen/ExerciseDetailsConfiguratorScreen.kt
+++ app/src/main/java/com/koleff/kare_android/ui/compose/screen/ExerciseDetailsConfiguratorScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -109,7 +110,7 @@ fun ExerciseDetailsConfiguratorScreen(
     }
 
     //Switch state
-    var restBetweenSetsIsChecked by remember {
+    var restBetweenSetsIsChecked by rememberSaveable {
         mutableStateOf(false)
     }
 
