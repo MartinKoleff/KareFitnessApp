5bfc637071a31ff5f15da3473cc92656bfdd8387
Martin Kolev <martin.kolev@iteco.bg>
2025-04-03 10:00:20 +0300

    Updated workout local data source. Updated statistics dao.

--- app/src/main/java/com/koleff/kare_android/data/room/dao/StatisticsDao.kt
+++ app/src/main/java/com/koleff/kare_android/data/room/dao/StatisticsDao.kt
@@ -2,24 +2,28 @@ package com.koleff.kare_android.data.room.dao
 
 import androidx.room.Dao
 import androidx.room.Query
-import com.koleff.kare_android.data.model.dto.MuscleGroup
-import com.koleff.kare_android.data.model.dto.MuscleGroupMaxWeight
 import com.koleff.kare_android.data.model.dto.WeightProgression
-import com.koleff.kare_android.data.room.entity.ExerciseWithSets
 import com.koleff.kare_android.data.room.entity.Workout
 
 @Dao
 interface StatisticsDao {
 
+
     //Exercise specific
-    @Query("SELECT MAX(weight) FROM do_workout_exercise_set WHERE reps = 1 & exerciseId = :exerciseId")
+    @Query("SELECT MAX(weight) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId")
     suspend fun getBestPR(exerciseId: Int): Float?
 
-    @Query("SELECT COUNT(reps) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId")
+    @Query("SELECT MAX(weight) FROM do_workout_exercise_set WHERE reps = 1 AND exerciseId = :exerciseId")
+    suspend fun get1RepMax(exerciseId: Int): Float?
+
+    @Query("SELECT SUM(reps) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId")
     suspend fun getTotalRepsPerformedForExercise(exerciseId: Int): Int?
 
     @Query("SELECT COUNT(*) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId")
     suspend fun getTotalSetsPerformedForExercise(exerciseId: Int): Int?
+
+    @Query("SELECT SUM(reps * weight) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId")
+    suspend fun getTotalWeightLiftedForExercise(exerciseId: Int): Float?
 
 
     //Workout specific stats
@@ -32,16 +36,13 @@ interface StatisticsDao {
     @Query("SELECT weight, date FROM do_workout_exercise_set WHERE exerciseId = :exerciseId ORDER BY date ASC")
     suspend fun getWeightProgression(exerciseId: Int): List<WeightProgression>?
 
-    @Query("SELECT COUNT(reps) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId & workoutId = :workoutId")
-    suspend fun getTotalRepsPerformedForExercise(exerciseId: Int, workoutId: Int): Int?
+    @Query("SELECT SUM(reps) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId AND workoutId = :workoutId")
+    suspend fun getTotalRepsPerformedForExercise(exerciseId: Int, workoutId: Int): Int? //Not working...
 
-    @Query("SELECT COUNT(*) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId & workoutId = :workoutId")
+    @Query("SELECT COUNT(*) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId AND workoutId = :workoutId")
     suspend fun getTotalSetsPerformedForExercise(exerciseId: Int, workoutId: Int): Int?
 
-    @Query("SELECT COUNT(reps * weight) FROM do_workout_exercise_set WHERE exerciseId = :exerciseId")
-    suspend fun getTotalWeightLiftedForExercise(exerciseId: Int): Float?
-
-    @Query("SELECT COUNT(reps * weight) FROM do_workout_exercise_set WHERE workoutId = :workoutId")
+    @Query("SELECT SUM(reps * weight) FROM do_workout_exercise_set WHERE workoutId = :workoutId")
     suspend fun getTotalWeightLiftedForWorkout(workoutId: Int): Float?
 
 
@@ -52,37 +53,14 @@ interface StatisticsDao {
     @Query("SELECT DISTINCT COUNT(exerciseId) FROM do_workout_exercise_set")
     suspend fun getTotalExercisesCount(): Int?
 
-    @Query("""
-    SELECT e.*, d.*
-    FROM do_workout_exercise_set d
-    JOIN exercise_table e ON d.exerciseId = e.exerciseId
-    GROUP BY d.exerciseId
-    ORDER BY COUNT(d.exerciseId) DESC
-    LIMIT 1
-    """)
-    suspend fun getMostPerformedExercise(): ExerciseWithSets?
-
-    @Query("SELECT muscleGroup FROM do_workout_exercise_set JOIN workout_table w ON do_workout_exercise_set.workoutId = w.workoutId GROUP BY w.muscleGroup ORDER BY COUNT(w.muscleGroup) DESC LIMIT 1")
-    suspend fun getFavoriteMuscleGroup(): MuscleGroup?
-
     @Query("SELECT COUNT(*) FROM do_workout_exercise_set")
     suspend fun getTotalSetsCount(): Int?
 
-    @Query("SELECT COUNT(reps) FROM do_workout_exercise_set")
+    @Query("SELECT SUM(reps) FROM do_workout_exercise_set")
     suspend fun getTotalRepsCount(): Int?
 
-    @Query("SELECT COUNT(reps * weight) FROM do_workout_exercise_set")
+    @Query("SELECT SUM(reps * weight) FROM do_workout_exercise_set")
     suspend fun getTotalWeightLifted(): Float?
-
-    @Query("""
-    SELECT e.muscleGroup AS muscleGroup, MAX(d.weight) AS maxWeight
-    FROM do_workout_exercise_set d
-    INNER JOIN exercise_table e ON e.exerciseId = d.exerciseId
-    GROUP BY e.muscleGroup
-    ORDER BY COUNT(d.weight) DESC
-    LIMIT 1
-    """)
-    suspend fun getMaxWeightPerMuscleGroup(): MuscleGroupMaxWeight?
 
     @Query("SELECT MAX(weight) FROM do_workout_exercise_set WHERE reps = 1")
     suspend fun getBestPR(): Float?
