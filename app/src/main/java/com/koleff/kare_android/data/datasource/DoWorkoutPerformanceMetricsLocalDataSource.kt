337d31e45d095a9ccc37660d791810bd67263961
Martin Kolev <martin.kolev@iteco.bg>
2025-04-23 14:36:09 +0300

    Fixed workout details deleted on workout start.

--- app/src/main/java/com/koleff/kare_android/data/datasource/DoWorkoutPerformanceMetricsLocalDataSource.kt
+++ app/src/main/java/com/koleff/kare_android/data/datasource/DoWorkoutPerformanceMetricsLocalDataSource.kt
@@ -32,7 +32,6 @@ class DoWorkoutPerformanceMetricsLocalDataSource(
             delay(Constants.fakeDelay)
 
             val id = doWorkoutPerformanceMetricsDao.insertWorkoutPerformanceMetrics(performanceMetrics.toEntity()).toInt()
-            workoutDao.insertWorkout(performanceMetrics.workout.toEntity()) //With new relations its no longer needed...
 
             val result = DoWorkoutPerformanceMetricsWrapper(
                 DoWorkoutPerformanceMetricsResponse(
